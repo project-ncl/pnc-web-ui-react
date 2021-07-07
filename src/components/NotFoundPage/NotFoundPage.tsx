25e83329c788018cd275347ce8c5bca8dd3135b0
Martin Kelnar <mkelnar@redhat.com>
2021-07-07 10:16:34 +0200

    Create Not Found page

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import { Title, EmptyState, EmptyStateIcon, EmptyStateBody, EmptyStateVariant } from '@patternfly/react-core';
+import { CubesIcon } from '@patternfly/react-icons';
+
+export const NotFoundPage = () => {
+  return (
+    <EmptyState variant={EmptyStateVariant.xl} className="pf-u-pt-4xl">
+      <EmptyStateIcon icon={CubesIcon} />
+      <Title headingLevel="h1" size="4xl">
+        404: That page no longer exists
+      </Title>
+      <EmptyStateBody>The requested resource could not be found.</EmptyStateBody>
+
+      <Link to="/" className="pf-c-button pf-m-primary">
+        Return to Dashboard
+      </Link>
+    </EmptyState>
+  );
+};
