415da681cf09bd8b1a2aada57106454123bf1976
Dennis Zhou <tozhou@redhat.com>
2021-07-19 13:45:55 +0100

    [NCL-6628] Fix unit test issue for components with router

--- src/components/NotFoundPage/__tests__/NotFoundPage.test.tsx
+++ src/components/NotFoundPage/__tests__/NotFoundPage.test.tsx
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react';
 import { NotFoundPage } from '../NotFoundPage';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 test('renders NotFoundPage', () => {
-  // render(<NotFoundPage />);
+  render(
+    <MemoryRouter>
+      <NotFoundPage />
+    </MemoryRouter>
+  );
 });
--- src/components/ProjectsPage/__tests__/ProjectsPage.test.tsx
+++ src/components/ProjectsPage/__tests__/ProjectsPage.test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import { ProjectsPage } from '../ProjectsPage';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders ProjectsPage', () => {
-  //   render(<ProjectsPage />);
+  render(
+    <MemoryRouter>
+      <ProjectsPage />
+    </MemoryRouter>
+  );
 });
