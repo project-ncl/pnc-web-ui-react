eec9b4b97bb194240acfe97c8539cc6a63b98eb0
Martin Kelnar <mkelnar@redhat.com>
2021-11-04 14:24:45 +0100

    NCL-6753 Empty, Error and Loading state components

---
+++ src/components/EmptyStates/EmptyStateCard.tsx
@@ -0,0 +1,15 @@
+import { CubesIcon } from '@patternfly/react-icons';
+import { StateCard } from './StateCard';
+
+interface IEmptyStateCard {
+  title: string;
+}
+
+/**
+ * Dump component representing empty state.
+ *
+ * @param title - Title subject, for example "Project List"
+ */
+export const EmptyStateCard = ({ title }: IEmptyStateCard) => (
+  <StateCard title={`${title} contains no data`} icon={CubesIcon}></StateCard>
+);
---
+++ src/components/EmptyStates/LoadingStateCard.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { StateCard } from './StateCard';
+
+const Spinner = () => (
+  <span className="pf-c-spinner" role="progressbar" aria-valuetext="Loading...">
+    <span className="pf-c-spinner__clipper" />
+    <span className="pf-c-spinner__lead-ball" />
+    <span className="pf-c-spinner__tail-ball" />
+  </span>
+);
+
+interface ILoadingStateCard {
+  delayMilliseconds?: number;
+  title: string;
+}
+
+/**
+ * Loading Empty State component with delayed render. It will be displayed after defined waiting time
+ * to prevent flashing user experience when spinner is displayed and almost immediately replaced.
+ *
+ * @param delayMilliseconds - Waiting time before component gets rendered
+ * @param title - Title subject, for example "Project List"
+ */
+export const LoadingStateCard = ({ delayMilliseconds = 750, title }: ILoadingStateCard) => {
+  const [show, setShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), delayMilliseconds);
+
+    return () => clearTimeout(timer);
+  }, [delayMilliseconds]);
+
+  if (show) {
+    return <StateCard title={`Loading ${title}`} icon={Spinner}></StateCard>;
+  }
+  return null;
+};
