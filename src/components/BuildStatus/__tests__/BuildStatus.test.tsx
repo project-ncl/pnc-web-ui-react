6ad53d3e2994153a5ffa950b48170eb51680107e
jakubvanko <kubko.vanko@gmail.com>
2021-11-09 22:53:57 +0100

    [NCL-6692] Fix BuildStatus tests

--- src/components/BuildStatus/__tests__/BuildStatus.test.tsx
+++ src/components/BuildStatus/__tests__/BuildStatus.test.tsx
@@ -1,14 +1,17 @@
 import { render } from '@testing-library/react';
 import { BuildStatusType } from '../../../scripts/Build';
+import { BrowserRouter } from 'react-router-dom';
 import { BuildStatus } from '../BuildStatus';
 
 test('renders BuildStatus', () => {
   render(
-    <BuildStatus
-      identifier="20180911-1037"
-      status={BuildStatusType.CANCELLED}
-      user="jvanko"
-      date={new Date(2021, 11, 3, 10, 24, 43, 13)}
-    />
+    <BrowserRouter basename="/pnc-web">
+      <BuildStatus
+        identifier="20180911-1037"
+        status={BuildStatusType.CANCELLED}
+        user="jvanko"
+        date={new Date(2021, 11, 3, 10, 24, 43, 13)}
+      />
+    </BrowserRouter>
   );
 });
