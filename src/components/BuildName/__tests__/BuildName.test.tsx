601ef157a870979aa3ab7b7b5c2e252be996c92b
jakubvanko <kubko.vanko@gmail.com>
2021-11-09 22:53:57 +0100

    [NCL-6692] Fix BuildStatus tests

--- src/components/BuildName/__tests__/BuildName.test.tsx
+++ src/components/BuildName/__tests__/BuildName.test.tsx
@@ -1,9 +1,18 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { BuildName } from '../BuildName';
 
 test('renders BuildName', () => {
   render(<BuildName identifier="test" />);
-  render(<BuildName identifier="test" link="/test.com" />);
+  render(
+    <BrowserRouter basename="/pnc-web">
+      <BuildName identifier="test" link="/test.com" />
+    </BrowserRouter>
+  );
   render(<BuildName identifier="test" additionalIdentifier="test2" />);
-  render(<BuildName identifier="test" additionalIdentifier="test2" link="/test.com" additionalLink="/test2.com" />);
+  render(
+    <BrowserRouter basename="/pnc-web">
+      <BuildName identifier="test" additionalIdentifier="test2" link="/test.com" additionalLink="/test2.com" />
+    </BrowserRouter>
+  );
 });
