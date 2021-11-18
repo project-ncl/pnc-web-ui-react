f613b9ab5b8f0086eaac553c8655b917e851a928
Dennis Zhou <tozhou@redhat.com>
2021-11-18 00:42:58 +0000

    [NCL-6755] Create first unit test for Project List

--- src/components/ProjectsList/__tests__/ProjectsList.test.tsx
+++ src/components/ProjectsList/__tests__/ProjectsList.test.tsx
@@ -1,13 +1,43 @@
 import { ProjectsList } from '../ProjectsList';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
-// temporarily disabled
-test('renders ProjectsList', () => {
-  render(
-    <></>
-    // <MemoryRouter>
-    //   <ProjectsList />
-    // </MemoryRouter>
-  );
+describe('display ProjectList component', () => {
+  let mockProjectsRequest: any;
+  let mockProjects: any;
+
+  async function loadMocks() {
+    mockProjectsRequest = await import('./data/mock-projects-request.json');
+    mockProjects = mockProjectsRequest.content;
+  }
+
+  beforeEach(async () => {
+    await loadMocks();
+  });
+
+  test('renders ProjectList to have the right data', () => {
+    render(
+      <MemoryRouter>
+        <ProjectsList projects={mockProjects} />
+      </MemoryRouter>
+    );
+    const firstProject = screen.getByText(mockProjects[0].name);
+    expect(firstProject).toBeInTheDocument();
+    const lastProject = screen.getByText(mockProjects[6].description);
+    expect(lastProject).toBeInTheDocument();
+    const lastProjectCount = screen.getByText(17);
+    expect(lastProjectCount).toBeInTheDocument();
+  });
+
+  test('compare snapshot with previous record', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <ProjectsList projects={mockProjects} />
+        </MemoryRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
