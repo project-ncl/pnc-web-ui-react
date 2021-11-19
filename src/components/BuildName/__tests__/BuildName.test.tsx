af53bd2f1743280e066e555ba1b120a0d5ad46f5
jakubvanko <kubko.vanko@gmail.com>
2021-11-19 23:39:58 +0100

    [NCL-6692] Refactor BuildName to support GroupBuilds

--- src/components/BuildName/__tests__/BuildName.test.tsx
+++ src/components/BuildName/__tests__/BuildName.test.tsx
@@ -1,18 +1,18 @@
 import { render } from '@testing-library/react';
+import { Build, GroupBuild } from 'pnc-api-types-ts';
 import { BrowserRouter } from 'react-router-dom';
 import { BuildName } from '../BuildName';
 
-test('renders BuildName', () => {
-  render(<BuildName name="test" />);
-  render(
-    <BrowserRouter basename="/pnc-web">
-      <BuildName name="test" link="/test.com" />
-    </BrowserRouter>
-  );
-  render(<BuildName name="test" additionalIdentifier="test2" />);
-  render(
-    <BrowserRouter basename="/pnc-web">
-      <BuildName name="test" additionalIdentifier="test2" link="/test.com" additionalLink="/test2.com" />
-    </BrowserRouter>
-  );
+import mockBuildData from './data/mock-build-data.json';
+
+describe('display BuildName component', () => {
+  test('renders all BuildName variants', () => {
+    (mockBuildData as (Build | GroupBuild)[]).forEach((build) =>
+      render(
+        <BrowserRouter>
+          <BuildName build={build} long includeBuildLink includeConfigLink />
+        </BrowserRouter>
+      )
+    );
+  });
 });
