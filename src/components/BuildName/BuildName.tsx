533bf977e9b8ba640a637cf135f75a5af1238cf9
jakubvanko <kubko.vanko@gmail.com>
2021-11-22 15:50:30 +0100

    [NCL-6692] Add documentation

--- src/components/BuildName/BuildName.tsx
+++ src/components/BuildName/BuildName.tsx
@@ -30,6 +30,19 @@ interface IBuildName {
   includeConfigLink?: boolean;
 }
 
+/**
+ * Represents a component for displaying the name of a build/groupBuild
+ * There are two versions: short (default) and long
+ *
+ * @remarks
+ * Long version additionally also includes the name of the build configuration
+ * Both "short" and "long" versions can also contain links to the actual build/config
+ *
+ * @param build - Build or GroupBuild
+ * @param long - Whether the component should be of the long version
+ * @param includeBuildLink - Whether the build name should be a link to the build page
+ * @param includeConfigLink - Whether the build config (in the long version) should be a link to the build config page
+ */
 export const BuildName = ({ build, long, includeBuildLink, includeConfigLink }: IBuildName) => {
   const name = calculateBuildName(build);
   const configName =
