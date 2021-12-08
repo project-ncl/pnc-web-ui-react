b864d2a5f90cef7bb6846473a1abcbcf2fde41d6
Dennis Zhou <tozhou@redhat.com>
2021-12-08 11:07:55 +0000

    [NCL-6818] Remove ts docs for mock class

--- src/services/__mocks__/projectService.ts
+++ src/services/__mocks__/projectService.ts
@@ -1,9 +1,4 @@
 class ProjectServiceMock {
-  /**
-   * Gets all Projects.
-   *
-   * @returns Projects
-   */
   public getProjects() {
     return new Promise((resolve) => {
       import('./projects-mock.json').then((mockProjectsRequest) => {
@@ -12,28 +7,13 @@ class ProjectServiceMock {
     });
   }
 
-  /**
-   * Gets a specific Project.
-   *
-   * @param id - ID of the Project
-   * @returns Project
-   */
   public getProject(id: string) {
     throw new Error('getProject: Not implemented yet');
   }
 
-  /**
-   * Gets all builds associated with a specific project.
-   *
-   * @param id - ID of the Project
-   * @returns Builds.
-   */
   public getProjectBuilds(id: string) {
     throw new Error('getProjectBuilds: Not implemented yet');
   }
 }
 
-/**
- * Instance of ProjectService providing group of Project related API operations.
- */
 export const projectService = new ProjectServiceMock();
