113373ec7c6bfe41d916c46a04d647e84d588b93
Dennis Zhou <tozhou@redhat.com>
2021-12-21 02:22:32 +0000

    [NCL-6847] Seperate milestone and release test data

--- src/components/MilestoneReleaseLabel/__test__/MilestoneReleaseLabel.test.tsx
+++ src/components/MilestoneReleaseLabel/__test__/MilestoneReleaseLabel.test.tsx
@@ -1,37 +1,37 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { ProductMilestone, ProductRelease } from 'pnc-api-types-ts';
 import { MilestoneReleaseLabel } from '../MilestoneReleaseLabel';
-import mockMilestoneReleaseData from './data/mock-milestone-release-data.json';
+import productMilestoneMock from './data/product-milestones-mock.json';
+import productReleaseMock from './data/product-releases-mock.json';
 
 describe('display MilestoneReleaseLabel component', () => {
   test('renders MilestoneReleaseLabels', () => {
-    let testDataList = mockMilestoneReleaseData as (ProductMilestone | ProductRelease)[];
-    let data1 = testDataList[0] as ProductMilestone;
-    let data2 = testDataList[1] as ProductMilestone;
-    let data3 = testDataList[2] as ProductRelease;
+    let testMilestoneDataList = productMilestoneMock as unknown as ProductMilestone[];
+    let testReleaseDataList = productReleaseMock as unknown as ProductRelease[];
     render(
       <>
-        <MilestoneReleaseLabel milestoneRelease={data1} isCurrent={false}></MilestoneReleaseLabel>
-        <MilestoneReleaseLabel milestoneRelease={testDataList[1]} isCurrent={true}></MilestoneReleaseLabel>
-        <MilestoneReleaseLabel milestoneRelease={testDataList[2]} isCurrent={false}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testMilestoneDataList[0]} isCurrent={false}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testMilestoneDataList[1]} isCurrent={true}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testReleaseDataList[0]} isCurrent={false}></MilestoneReleaseLabel>
       </>
     );
-    const firstLabel = screen.getByText(data1.version as string);
-    const secondLabel = screen.getByText(data2.version as string);
-    const thirdLabel = screen.getByText(data3.version as string);
+    const firstLabel = screen.getByText(testMilestoneDataList[0].version as string);
+    const secondLabel = screen.getByText(testMilestoneDataList[1].version as string);
+    const thirdLabel = screen.getByText(testReleaseDataList[0].version as string);
     expect(firstLabel).toHaveClass('milestone-label');
     expect(secondLabel).toHaveClass('milestone-label is-current');
     expect(thirdLabel).toHaveClass('release-label');
   });
 
   test('compare snapshot with previous record', () => {
-    let testDataList = mockMilestoneReleaseData as (ProductMilestone | ProductRelease)[];
+    let testMilestoneDataList = productMilestoneMock as unknown as ProductMilestone[];
+    let testReleaseDataList = productReleaseMock as unknown as ProductRelease[];
 
     const tree = render(
       <>
-        <MilestoneReleaseLabel milestoneRelease={testDataList[0]} isCurrent={false}></MilestoneReleaseLabel>
-        <MilestoneReleaseLabel milestoneRelease={testDataList[1]} isCurrent={true}></MilestoneReleaseLabel>
-        <MilestoneReleaseLabel milestoneRelease={testDataList[2]} isCurrent={false}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testMilestoneDataList[0]} isCurrent={false}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testMilestoneDataList[1]} isCurrent={true}></MilestoneReleaseLabel>
+        <MilestoneReleaseLabel milestoneRelease={testReleaseDataList[0]} isCurrent={false}></MilestoneReleaseLabel>
       </>
     );
     expect(tree).toMatchSnapshot();
