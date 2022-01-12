f27c2e5e7f025bdc0dde7022c70595f75b76a111
Dennis Zhou <tozhou@redhat.com>
2022-01-12 01:38:17 +0000

    [NCL-6847] Change css to CSS module

---
+++ src/components/ProductMilestoneReleaseLabel/__test__/ProductMilestoneReleaseLabel.test.tsx
@@ -0,0 +1,57 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { ProductMilestone, ProductRelease } from 'pnc-api-types-ts';
+import { ProductMilestoneReleaseLabel } from '../ProductMilestoneReleaseLabel';
+import productMilestoneMock from './data/product-milestones-mock.json';
+import productReleaseMock from './data/product-releases-mock.json';
+
+describe('display MilestoneReleaseLabel component', () => {
+  test('renders MilestoneReleaseLabels', () => {
+    let testMilestoneDataList = productMilestoneMock as unknown as ProductMilestone[];
+    let testReleaseDataList = productReleaseMock as unknown as ProductRelease[];
+    render(
+      <>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testMilestoneDataList[0]}
+          isCurrent={false}
+        ></ProductMilestoneReleaseLabel>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testMilestoneDataList[1]}
+          isCurrent={true}
+        ></ProductMilestoneReleaseLabel>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testReleaseDataList[0]}
+          isCurrent={false}
+        ></ProductMilestoneReleaseLabel>
+      </>
+    );
+    const firstLabel = screen.getByText(testMilestoneDataList[0].version as string);
+    const secondLabel = screen.getByText(testMilestoneDataList[1].version as string);
+    const thirdLabel = screen.getByText(testReleaseDataList[0].version as string);
+    expect(firstLabel).toHaveClass('milestoneLabel');
+    expect(secondLabel).toHaveClass('milestoneLabel isCurrent');
+    expect(thirdLabel).toHaveClass('releaseLabel');
+  });
+
+  test('compare snapshot with previous record', () => {
+    let testMilestoneDataList = productMilestoneMock as unknown as ProductMilestone[];
+    let testReleaseDataList = productReleaseMock as unknown as ProductRelease[];
+
+    const tree = render(
+      <>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testMilestoneDataList[0]}
+          isCurrent={false}
+        ></ProductMilestoneReleaseLabel>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testMilestoneDataList[1]}
+          isCurrent={true}
+        ></ProductMilestoneReleaseLabel>
+        <ProductMilestoneReleaseLabel
+          productMilestoneRelease={testReleaseDataList[0]}
+          isCurrent={false}
+        ></ProductMilestoneReleaseLabel>
+      </>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
