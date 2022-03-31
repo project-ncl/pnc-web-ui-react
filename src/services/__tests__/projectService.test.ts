73ee13754167fa8b8896eb50ffc5db115c9757bf
Martin Kelnar <mkelnar@redhat.com>
2022-03-31 18:57:56 +0200

    NCL-6978 Fix projectService unit tests

--- src/services/__tests__/projectService.test.ts
+++ src/services/__tests__/projectService.test.ts
@@ -10,13 +10,13 @@ describe('test all functions of the project service', () => {
   });
 
   test('get project', async () => {
-    let res = await projectService.getProject('106');
+    let res = await projectService.getProject({ id: '106' });
     expect(res.status).toBe(200);
     expect(res.data.id).toBe('106');
   });
 
   test('get project builds', async () => {
-    let res = await projectService.getProjectBuilds('106');
+    let res = await projectService.getProjectBuilds({ id: '106' });
     expect(res.status).toBe(200);
     expect(res.data.content.length).toBe(3);
   });
