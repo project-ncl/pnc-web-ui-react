daa696f074f1f7b94f6ad5943760983df353cccd
Dennis Zhou <tozhou@redhat.com>
2022-04-26 14:17:25 +0100

    [NCL-6794] Add Build Metrics component

--- src/components/DemoPage/DemoPage.tsx
+++ src/components/DemoPage/DemoPage.tsx
@@ -1,9 +1,14 @@
 import { PageLayout } from '../PageLayout/PageLayout';
 import { Card, CardTitle, CardBody, Flex, FlexItem } from '@patternfly/react-core';
+import { Build } from 'pnc-api-types-ts';
 import { BuildStatus } from '../BuildStatus/BuildStatus';
 import { BuildStatusIcon } from '../BuildStatusIcon/BuildStatusIcon';
+import { BuildMetrics } from '../BuildMetrics/BuildMetrics';
 import { BuildName } from '../BuildName/BuildName';
 import { ProductMilestoneReleaseLabel } from '../ProductMilestoneReleaseLabel/ProductMilestoneReleaseLabel';
+import mockBuildData from './data/mock-build-data.json';
+
+const buildRes: Build[] = mockBuildData;
 
 export const DemoPage = () => {
   return (
@@ -279,6 +284,16 @@ export const DemoPage = () => {
                 }}
                 isCurrent={true}
               />
+            </CardBody>
+          </Card>
+        </FlexItem>
+        <FlexItem>
+          <Card>
+            <CardTitle>BuildMetrics</CardTitle>
+            <CardBody>
+              <BuildMetrics builds={buildRes} chartType="line" componentId="BMTEST1"></BuildMetrics>
+              <br />
+              <BuildMetrics builds={buildRes} chartType="horizontalBar" componentId="BMTEST2"></BuildMetrics>
             </CardBody>
           </Card>
         </FlexItem>
