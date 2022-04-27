28363609b91d1f9c43a7e52994ccfc3de9cada2e
jakubvanko <kubko.vanko@gmail.com>
2022-04-27 14:39:12 +0200

    [NCL-6583] Add documentation

--- src/components/ProjectLink/ProjectLink.tsx
+++ src/components/ProjectLink/ProjectLink.tsx
@@ -5,4 +5,10 @@ interface IProjectLinkProps {
   children?: React.ReactNode;
 }
 
+/**
+ * Represents a link to a ProjectDetailPage of a specific project
+ *
+ * @param id - id of the project to link to
+ * @param children - the children of the link
+ */
 export const ProjectLink = ({ id, children }: IProjectLinkProps) => <Link to={id} children={children} />;
