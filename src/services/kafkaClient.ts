09c98e0235c080157b8373119c10c0f21ebddc81
Dennis Zhou <tozhou@redhat.com>
2022-05-25 03:01:42 +0100

    [NCL-6956] Add build Metrics component

---
+++ src/services/kafkaClient.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosInstance } from 'axios';
+import * as WebConfigAPI from './WebConfigService';
+
+/**
+ * Utils class managing http client instance, only one instance is created.
+ */
+class KafkaClient {
+  private httpClient: AxiosInstance;
+  public mathRandom: number = Math.random(); // development and testing purposes
+
+  constructor() {
+    this.httpClient = this.createHttpClient();
+  }
+
+  /**
+   * Creates Axios instance and configure interceptors.
+   *
+   * @returns Axios instance
+   */
+  private createHttpClient = (): AxiosInstance => {
+    const httpClient = axios.create({
+      baseURL: WebConfigAPI.getKafkaUrl(),
+    });
+
+    return httpClient;
+  };
+
+  // PUBLIC
+
+  /**
+   * @returns Axios instance
+   */
+  public getHttpClient(): AxiosInstance {
+    return this.httpClient;
+  }
+}
+
+export const kafkaClient = new KafkaClient();
