c5649cf7fbee86b92aa5f3a8eff2e77b601165dd
jakubvanko <kubko.vanko@gmail.com>
2022-06-22 16:02:32 +0200

    [NCL-7028] Add build status count to AdministrationPage

--- src/components/AdministrationPage/AdministrationPage.tsx
+++ src/components/AdministrationPage/AdministrationPage.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import {
   Button,
   Card,
@@ -12,9 +12,16 @@ import {
   Switch,
   TextArea,
   TextInput,
+  useInterval,
 } from '@patternfly/react-core';
 import { PageLayout } from './../PageLayout/PageLayout';
 import { useTitle } from '../../containers/useTitle';
+import { AttributesItems } from '../AttributesItems/AttributesItems';
+import { IService, useDataContainer } from '../../containers/DataContainer/useDataContainer';
+import { buildService } from '../../services/buildService';
+import { DataContainer } from '../../containers/DataContainer/DataContainer';
+
+const REFRESH_INTERVAL_SECONDS = 90;
 
 export const AdministrationPage = () => {
   const [isMaintenanceModeOn, setIsMaintenanceModeOn] = useState(false);
@@ -24,11 +31,23 @@ export const AdministrationPage = () => {
     paddingRight: '8px',
     paddingBottom: '5px',
   };
+  const [secondsUntilReload, setSecondsUntilReload] = useState(REFRESH_INTERVAL_SECONDS);
+  const dataContainer = useDataContainer(({ requestConfig }: IService) => buildService.getBuildCount(requestConfig));
+
+  // TODO: Create a better solution than disabling the next line
+  useEffect(() => dataContainer.refresh({ requestConfig: {} }), []); // eslint-disable-line react-hooks/exhaustive-deps
+  useInterval(() => {
+    setSecondsUntilReload(secondsUntilReload - 1);
+    if (secondsUntilReload <= 1) {
+      dataContainer.refresh({ requestConfig: {} });
+      setSecondsUntilReload(REFRESH_INTERVAL_SECONDS);
+    }
+  }, 1000);
 
   useTitle('Administration');
 
   return (
-    <PageLayout title="Administration" description="Administration tools for admin users">
+    <PageLayout title="Administration" description={'Administration tools for admin users'}>
       <Flex direction={{ default: 'column' }}>
         <FlexItem>
           <Form isHorizontal>
@@ -49,6 +68,45 @@ export const AdministrationPage = () => {
               </CardBody>
             </Card>
           </Form>
+        </FlexItem>
+        <FlexItem>
+          <Card>
+            <CardBody>
+              <Grid hasGutter>
+                <GridItem span={12}>
+                  <DataContainer {...dataContainer} title="Build Count">
+                    <AttributesItems
+                      attributes={[
+                        {
+                          name: 'Running builds count',
+                          value: dataContainer.data?.running,
+                        },
+                        {
+                          name: 'Enqueued builds count',
+                          value: dataContainer.data?.enqueued,
+                        },
+                        {
+                          name: 'Waiting for dependencies builds count',
+                          value: dataContainer.data?.waitingForDependencies,
+                        },
+                      ]}
+                    />
+                  </DataContainer>
+                </GridItem>
+                <GridItem span={4}>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      dataContainer.refresh({ requestConfig: {} });
+                      setSecondsUntilReload(REFRESH_INTERVAL_SECONDS);
+                    }}
+                  >
+                    Refresh ({secondsUntilReload} s)
+                  </Button>
+                </GridItem>
+              </Grid>
+            </CardBody>
+          </Card>
         </FlexItem>
         <FlexItem>
           <Form isHorizontal>
