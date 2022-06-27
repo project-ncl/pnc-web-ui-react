d86d092dedf83853ba73f0d2f0c8bf492b33f23b
jakubvanko <kubko.vanko@gmail.com>
2022-06-27 16:49:09 +0200

    [NCL-7028] Improve AdministrationPage tests by adding mocks

--- src/components/AdministrationPage/__tests__/AdministrationPage.test.tsx
+++ src/components/AdministrationPage/__tests__/AdministrationPage.test.tsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react';
 import { AdministrationPage } from '../AdministrationPage';
 
+jest.mock('../../../services/buildService');
+
 test('renders AdministrationPage', () => {
   render(<AdministrationPage />);
 });
