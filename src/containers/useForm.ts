47cf93d4f9cc62d017d28bfd1040a543bc8f4e46
Patrik Korytar <patrik.korytar@gmail.com>
2022-07-27 16:23:07 +0200

    NCL-7243 Add documentation of useForm hook

--- src/containers/useForm.ts
+++ src/containers/useForm.ts
@@ -12,20 +12,38 @@ interface IFieldValidators {
   [key: string]: Function;
 }
 
+/**
+ * Hook to manage input values, validation and states of a form. All validation is done on change of input.
+ * See also {@link ProjectCreateEditPage}
+ *
+ * @param initValues - Values to initialize inputs with
+ * @param validators - Functions to validate inputs with
+ * @param callback - Function to call when submitting user input data
+ *
+ * Both initValues and validators are objects whose keys are equal to ids of input elements.
+ */
 export const useForm = (initValues: IFieldValues, validators: IFieldValidators, callback: Function) => {
+  // are all form inputs valid?
   const [isFormValid, setIsFormValid] = useState<boolean>(false);
+  // has user made any changes to the newly loaded form?
+  // at least one change must be to in order to submit form
   const [hasChanged, setHasChanged] = useState<boolean>(false);
 
+  // input values
   const [fieldValues, setFieldValues] = useState<IFieldValues>(initValues);
+  // input error messages
   const [fieldErrors, setFieldErrors] = useState<IFieldErros>({});
+  // inpur validation functons
   const [fieldValidators, setFieldValidators] = useState<IFieldValidators>(validators);
 
+  // submitting state
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const initFieldStates = { ...initValues };
   Object.keys(initFieldStates).forEach((key) => {
     initFieldStates[key] = 'default';
   });
+  // input states - 'default' | 'success' | 'error'
   const [fieldStates, setFieldStates] = useState<any>(initFieldStates);
 
   useEffect(() => {
@@ -33,6 +51,7 @@ export const useForm = (initValues: IFieldValues, validators: IFieldValidators, 
       setIsFormValid(true);
       if (isSubmitting) {
         callback(fieldValues);
+        // reset state to 'default' (valid inputs wont be highlighted)
         setFieldStates(initFieldStates);
       }
     } else {
@@ -47,6 +66,7 @@ export const useForm = (initValues: IFieldValues, validators: IFieldValidators, 
     setFieldValues({ ...fieldValues, [fieldName]: event.currentTarget.value });
     setHasChanged(true);
 
+    // if has any validator
     if (fieldValidators[fieldName]) {
       const error = fieldValidators[fieldName](event.currentTarget.value);
       setError(fieldName, error);
@@ -66,18 +86,6 @@ export const useForm = (initValues: IFieldValues, validators: IFieldValidators, 
   };
 
   const onSubmit = () => {
-    for (const key in fieldValidators) {
-      const error = fieldValidators[key](fieldValues[key]);
-
-      if (error) {
-        setFieldErrors({ ...fieldErrors, [key]: error });
-      } else {
-        const newErrors = { ...fieldErrors };
-        delete newErrors[key];
-        setFieldErrors(newErrors);
-      }
-    }
-
     setIsSubmitting(true);
   };
 
