deabc342cc58a29533b1f3612612fee7198cf95e
Patrik Korytar <patrik.korytar@gmail.com>
2022-07-28 11:14:35 +0200

    NCL-7243 Fix project form to use new version of useForm

--- src/utils/formValidationHelpers.ts
+++ src/utils/formValidationHelpers.ts
@@ -1,14 +1,6 @@
-/**
- * Project name validation function.
- *
- * @param name - Project name
- * @returns empty string if valid, error message otherwise
- */
-
-export const validateProjectName = (name: String) => {
-  if (!name) return 'Name is required';
-  return '';
-};
+// url regex taken from:
+// https://uibakery.io/regex-library/url
+const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 
 /**
  * URL validation function.
@@ -18,13 +10,7 @@ export const validateProjectName = (name: String) => {
  * @param url - URL string
  * @returns empty string if valid, error message otherwise
  */
-
-// url regex taken from:
-// https://uibakery.io/regex-library/url
-
-const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-
 export const validateUrl = (url: string) => {
-  if (url && !urlRegex.test(url)) return 'Invalid URL';
+  if (url && !urlRegex.test(url)) return 'Invalid URL format';
   return '';
 };
