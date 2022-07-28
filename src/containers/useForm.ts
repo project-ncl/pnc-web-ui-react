271e0fc034fc512438ca27cc1edbcbdd4b5b1928
Patrik Korytar <patrik.korytar@gmail.com>
2022-07-28 11:13:44 +0200

    NCL-7243 Overhaul useForm and change documentation

--- src/containers/useForm.ts
+++ src/containers/useForm.ts
@@ -9,35 +9,50 @@ interface IFieldErrors {
 }
 
 interface IFieldValidators {
-  [key: string]: Function;
+  [key: string]: IValidator;
+}
+
+interface IValidator {
+  isRequired?: boolean;
+  validator?: Function;
 }
 
 /**
- * Hook to manage input values, validation and states of a form. All validation is done on change of input.
+ * Hook to manage input values, validation and states of a form.
+ * All validation is done on change of input.
+ * Submit button is firstly disabled. In order to enable button:
+ *  -> at least one change must be done
+ *  -> all required fields must not be empty
+ *  -> all validated inputs must be valid
+ *
  * See also {@link ProjectCreateEditPage}
  *
  * @param initValues - Values to initialize inputs with
- * @param validators - Functions to validate inputs with
+ * @param validators - Objects consisting of whether is input required and validation function
  * @param callback - Function to call when submitting user input data
  *
- * Both initValues and validators are objects whose keys are equal to ids of input elements.
+ * @returns form states and access functions
+ *  -> fieldValues - values of input fields
+ *  -> fieldErrors - error messages of inout fields
+ *  -> fieldStates - 'default' | 'success' | 'error'
+ *  -> isSubmitDisabled - submit disabled or not
+ *  -> onChange - callback for input fields on change
+ *  -> setFieldValues - set all field values
+ *  -> onSubmit - callback for submit button
+ *
+ * initValues, validators, fieldValues, fieldErrors, fieldStates are objects whose keys are equal to ids of inputs fields.
+ * example: validators.projectUrl.isRequired
  */
 export const useForm = (initValues: IFieldValues, validators: IFieldValidators, callback: Function) => {
-  // are all form inputs valid?
-  const [isFormValid, setIsFormValid] = useState<boolean>(false);
-  // has user made any changes to the newly loaded form?
-  // at least one change must be to in order to submit form
-  const [hasChanged, setHasChanged] = useState<boolean>(false);
+  // is submit button disabled?
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
 
   // input values
   const [fieldValues, setFieldValues] = useState<IFieldValues>(initValues);
   // input error messages
   const [fieldErrors, setFieldErrors] = useState<IFieldErrors>({});
-  // inpur validation functons
+  // inpur validation functions
   const [fieldValidators, setFieldValidators] = useState<IFieldValidators>(validators);
-
-  // submitting state
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const initFieldStates = { ...initValues };
   Object.keys(initFieldStates).forEach((key) => {
@@ -47,41 +62,64 @@ export const useForm = (initValues: IFieldValues, validators: IFieldValidators, 
   const [fieldStates, setFieldStates] = useState<any>(initFieldStates);
 
   useEffect(() => {
-    if (!Object.keys(fieldErrors).length && hasChanged) {
-      setIsFormValid(true);
-      if (isSubmitting) {
-        callback(fieldValues);
-        // reset state to 'default' (valid inputs wont be highlighted)
-        setFieldStates(initFieldStates);
-      }
+    if (isFormValid() && areRequiredFilled()) {
+      setIsSubmitDisabled(false);
     } else {
-      setIsFormValid(false);
+      setIsSubmitDisabled(true);
+    }
+  }, [fieldValues]);
+
+  // are all validated inputs valid?
+  const isFormValid = () => {
+    return !Object.keys(fieldErrors).length;
+  };
+
+  // are all required inputs filled?
+  const areRequiredFilled = () => {
+    for (const key in fieldValidators) {
+      if (fieldValidators[key].isRequired && !fieldValues[key]) {
+        return false;
+      }
     }
 
-    setIsSubmitting(false);
-  }, [fieldErrors, hasChanged]);
+    return true;
+  };
 
+  // callback (on change of input)
   const onChange = (event: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => {
     const fieldName = event.currentTarget.name;
     const fieldValue = event.currentTarget.value;
     setFieldValues({ ...fieldValues, [fieldName]: fieldValue });
-    setHasChanged(true);
 
-    // if has any validator
+    validate(fieldName, fieldValue);
+  };
+
+  // validate field
+  const validate = (fieldName: string, fieldValue: string) => {
     if (fieldValidators[fieldName]) {
-      const error = fieldValidators[fieldName](fieldValue);
-      setError(fieldName, fieldValue, error);
+      const isRequired = fieldValidators[fieldName].isRequired;
+      const validator = fieldValidators[fieldName].validator;
+      if (isRequired) {
+        const error = fieldValue ? '' : 'Field must be filled!';
+        setError(fieldName, fieldValue, error);
+      } else if (validator) {
+        const error = validator(fieldValue);
+        setError(fieldName, fieldValue, error);
+      }
     }
   };
 
+  // set error message and state
   const setError = (fieldName: string, fieldValue: string, error: string) => {
     if (error) {
       setFieldErrors({ ...fieldErrors, [fieldName]: error });
       setFieldStates({ ...fieldStates, [fieldName]: 'error' });
     } else {
+      // if no error, delete old error (if any)
       const newErrors = { ...fieldErrors };
       delete newErrors[fieldName];
       setFieldErrors(newErrors);
+      // display success state only if not empty
       if (fieldValue) {
         setFieldStates({ ...fieldStates, [fieldName]: 'success' });
       } else {
@@ -90,24 +128,13 @@ export const useForm = (initValues: IFieldValues, validators: IFieldValidators, 
     }
   };
 
+  // callback (on submit of form)
   const onSubmit = () => {
-    let errors: IFieldErrors = {};
-
-    // check all validated inputs before submit
-    for (const key in fieldValidators) {
-      const error = fieldValidators[key](fieldValues[key]);
-      if (error) errors[key] = error;
-    }
-
-    setFieldErrors(errors);
-    const fieldStates = { ...errors };
-    Object.keys(fieldStates).forEach((key) => {
-      fieldStates[key] = 'error';
-    });
-    setFieldStates(fieldStates);
-
-    setIsSubmitting(true);
+    callback(fieldValues);
+    // reset state to 'default' (valid inputs wont be highlighted)
+    setFieldStates(initFieldStates);
+    setIsSubmitDisabled(true);
   };
 
-  return { fieldValues, fieldErrors, fieldStates, isFormValid, onChange, setFieldValues, onSubmit };
+  return { fieldValues, fieldErrors, fieldStates, isSubmitDisabled, onChange, setFieldValues, onSubmit };
 };
