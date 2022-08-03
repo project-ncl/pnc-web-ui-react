b87f1866941e63247eabd0063552e0d5b2bb6c2f
Patrik Korytar <patrik.korytar@gmail.com>
2022-08-03 10:38:57 +0200

    NCL-7243 Implement example form on demo page

--- src/components/DemoPage/DemoPage.tsx
+++ src/components/DemoPage/DemoPage.tsx
@@ -1,5 +1,23 @@
 import { PageLayout } from '../PageLayout/PageLayout';
-import { Card, CardTitle, CardBody, Flex, FlexItem, Tooltip } from '@patternfly/react-core';
+import { useState } from 'react';
+import {
+  ActionGroup,
+  Button,
+  Card,
+  CardTitle,
+  CardBody,
+  Flex,
+  Form,
+  FormGroup,
+  FormHelperText,
+  FlexItem,
+  Select,
+  SelectOption,
+  SelectVariant,
+  Tooltip,
+  TextArea,
+  TextInput,
+} from '@patternfly/react-core';
 import { Build } from 'pnc-api-types-ts';
 import { BuildStatus } from '../BuildStatus/BuildStatus';
 import { BuildStatusIcon } from '../BuildStatusIcon/BuildStatusIcon';
@@ -12,14 +30,153 @@ import { InfoCircleIcon } from '@patternfly/react-icons';
 import { ActionButton } from '../ActionButton/ActionButton';
 import mockBuildData from './data/mock-build-data.json';
 import { useTitle } from '../../containers/useTitle';
+import { useForm } from '../../containers/useForm';
 
 const buildRes: Build[] = mockBuildData;
 
 export const DemoPage = () => {
-  useTitle('Demo');
+  useTitle('Demo Page');
+
+  const submitForm = () => {
+    console.log('form state when submitted:', {
+      ...form,
+    });
+  };
+
+  const { form, onChange, onSubmit, isSubmitDisabled } = useForm(
+    {
+      inputFieldA: {
+        value: '',
+        validator: { isRequired: true },
+      },
+      textAreaA: {
+        value: '',
+      },
+      selectA: {
+        value: '',
+        validator: { isRequired: true },
+      },
+    },
+    submitForm
+  );
+
+  const defaultSelectOptions = [{ value: 'Build' }, { value: 'Option' }, { value: 'Project' }, { value: 'Version' }];
+
+  const [selectOptions, setSelectOptions] = useState<any>(defaultSelectOptions);
+  const [isSelectOpen, setIsSelectOpen] = useState<boolean>(false);
+
+  const clearSelection = () => {
+    onChange('selectA', null);
+    setIsSelectOpen(false);
+    setSelectOptions(defaultSelectOptions);
+  };
+
+  const formComponent = (
+    <Card>
+      <CardBody>
+        <div className="w-70">
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <FormGroup
+              isRequired
+              label="Input Field"
+              fieldId="inputFieldA"
+              helperText={
+                <FormHelperText isHidden={form.inputFieldA.state !== 'error'} isError>
+                  {form.inputFieldA.errorMessage}
+                </FormHelperText>
+              }
+            >
+              <TextInput
+                isRequired
+                validated={form.inputFieldA.state}
+                type="text"
+                id="inputFieldA"
+                name="inputFieldA"
+                value={form.inputFieldA.value}
+                autoComplete="off"
+                onChange={(text) => {
+                  onChange('inputFieldA', text);
+                }}
+              />
+            </FormGroup>
+            <FormGroup label="Text Area" fieldId="textAreaA">
+              <TextArea
+                id="textAreaA"
+                name="textAreaA"
+                value={form.textAreaA.value}
+                onChange={(text) => {
+                  onChange('textAreaA', text);
+                }}
+                autoResize
+              />
+            </FormGroup>
+            <FormGroup
+              isRequired
+              label="Filtered Select"
+              fieldId="selectA"
+              helperText={
+                <FormHelperText isHidden={form.selectA.state !== 'error'} isError>
+                  {form.selectA.errorMessage}
+                </FormHelperText>
+              }
+            >
+              <Select
+                validated={form.selectA.state}
+                id="selectA"
+                variant={SelectVariant.typeahead}
+                typeAheadAriaLabel="Select an option"
+                onToggle={(isOpen) => {
+                  setIsSelectOpen(isOpen);
+                }}
+                onSelect={(event, selection, isPlaceholder) => {
+                  if (isPlaceholder) clearSelection();
+                  else {
+                    onChange('selectA', selection);
+                    setIsSelectOpen(false);
+                  }
+                }}
+                onClear={clearSelection}
+                selections={form.selectA.value}
+                isOpen={isSelectOpen}
+                aria-labelledby={'selectA'}
+                placeholderText="Select an option"
+              >
+                {selectOptions.map((option: any, index: any) => (
+                  <SelectOption key={index} value={option.value} />
+                ))}
+              </Select>
+            </FormGroup>
+            <ActionGroup>
+              <Button
+                variant="primary"
+                isDisabled={isSubmitDisabled}
+                onClick={() => {
+                  onSubmit();
+                }}
+              >
+                Submit
+              </Button>
+            </ActionGroup>
+          </Form>
+        </div>
+      </CardBody>
+    </Card>
+  );
+
   return (
     <PageLayout title="Component Demo" description="Component demo page intended for showcasing React components.">
       <Flex direction={{ default: 'column' }}>
+        <FlexItem>
+          <Card>
+            <CardTitle>Form Demo</CardTitle>
+            <CardBody>{formComponent}</CardBody>
+          </Card>
+        </FlexItem>
+
         <FlexItem>
           <Card>
             <CardTitle>BuildStatus</CardTitle>
@@ -295,6 +452,7 @@ export const DemoPage = () => {
             </CardBody>
           </Card>
         </FlexItem>
+
         <FlexItem>
           <Card>
             <CardTitle>BuildMetrics</CardTitle>
