1557318078c5706ba5eb676fba2d79a70a22a7cf
Patrik Korytar <patrik.korytar@gmail.com>
2022-08-08 13:09:34 +0200

    NCL-7243 Remove unused function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,19 +19,3 @@ export const createDateTime = ({ date, includeDate = true, includeTime = true }:
     ...(includeDate && { dateStyle: 'medium' }),
     ...(includeTime && { timeStyle: 'medium' }),
   }).format(typeof date === 'string' ? new Date(date) : date);
-
-/**
- * Copies a object, sets all keys to same value and returns it.
- *
- * @param originalObj - Object to copy.
- * @param value - Value to set all keys of a copy to.
- * @returns copied object with keys set to same value
- */
-export const copyAndSetValues = (originalObj: any, value: any) => {
-  const copiedObj = { ...originalObj };
-  Object.keys(copiedObj).forEach((key) => {
-    copiedObj[key] = value;
-  });
-
-  return copiedObj;
-};
