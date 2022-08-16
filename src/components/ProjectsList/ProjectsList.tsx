d6f2862407d7f7afe481168ccaaaa2de4a2e4060
Patrik Korytar <patrik.korytar@gmail.com>
2022-08-16 12:50:44 +0200

    NCL-7256 Create ProtectedContent component

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,12 +1,10 @@
-import '@patternfly/react-core/dist/styles/base.css';
 import '../../index.css';
-
+import { ProjectLink } from '../ProjectLink/ProjectLink';
+import { ProtectedContent } from '../ProtectedContent/ProtectedContent';
+import '@patternfly/react-core/dist/styles/base.css';
 import { Table, TableHeader, TableBody, cellWidth } from '@patternfly/react-table';
 import { Project } from 'pnc-api-types-ts';
 import { Link } from 'react-router-dom';
-import { keycloakService } from '../../services/keycloakService';
-
-import { ProjectLink } from '../ProjectLink/ProjectLink';
 
 interface IProjectsList {
   projects: Project[];
@@ -33,9 +31,9 @@ export const ProjectsList = ({ projects }: IProjectsList) => {
     Object.keys(project.buildConfigs || []).length,
     {
       title: (
-        <Link className={keycloakService.isKeycloakAvailable ? '' : 'disabled-content'} to={`${project.id}/edit`}>
-          edit
-        </Link>
+        <ProtectedContent>
+          <Link to={`${project.id}/edit`}>edit</Link>
+        </ProtectedContent>
       ),
     },
   ]);
