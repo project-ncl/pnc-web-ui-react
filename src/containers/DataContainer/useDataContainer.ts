cb6e0b75db380cf1950fab7876982827c1dcff96
Patrik Korytar <patrik.korytar@gmail.com>
2022-09-20 10:29:56 +0200

    NCL-7299 Provide abort function for useDataContainer

--- src/containers/DataContainer/useDataContainer.ts
+++ src/containers/DataContainer/useDataContainer.ts
@@ -32,6 +32,10 @@ export const useDataContainer = (service: Function, { initLoadingState = true }:
   const loadingCount = useRef<number>(0);
   const lastAbortController = useRef<AbortController>();
 
+  const abortService = () => {
+    lastAbortController.current?.abort();
+  };
+
   const invokeService = ({ serviceData = null, requestConfig = {} }: IService<any>) => {
     loadingCount.current++;
 
@@ -89,5 +93,6 @@ export const useDataContainer = (service: Function, { initLoadingState = true }:
     loading: loading,
     error: error,
     refresh: useCallback(invokeService, [service]),
+    abort: useCallback(abortService, []),
   };
 };
