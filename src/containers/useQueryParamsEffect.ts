60778f5ca5ba1e69a2de22dae8b7ee14cfddee73
Martin Kelnar <mkelnar@redhat.com>
2022-10-12 11:22:56 +0200

    NCL-7382 Use mandatory parameters to prevent unnecessary HTTP requests

--- src/containers/useQueryParamsEffect.ts
+++ src/containers/useQueryParamsEffect.ts
@@ -1,22 +1,49 @@
 import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { getComponentQueryParamsObject, queryParamsObjectsAreEqual } from '../utils/queryParamsHelper';
+import { IQueryParamsObject, getComponentQueryParamsObject, queryParamsObjectsAreEqual } from '../utils/queryParamsHelper';
 
-export const useQueryParamsEffect = (service: Function, componentId: string) => {
+interface IMandatoryParams {
+  pagination?: boolean;
+  sorting?: boolean;
+}
+
+const defaultMandatoryParams: IMandatoryParams = { pagination: true, sorting: false };
+
+const areMandatoryParamsAvailable = (mandatoryParams: IMandatoryParams, componentQueryParamsObject: IQueryParamsObject) => {
+  if (mandatoryParams.pagination && (!componentQueryParamsObject.pageIndex || !componentQueryParamsObject.pageSize)) {
+    return false;
+  }
+
+  if (mandatoryParams.sorting && !componentQueryParamsObject.sort) {
+    return false;
+  }
+
+  return true;
+};
+
+export const useQueryParamsEffect = (
+  service: Function,
+  componentId: string,
+  mandatoryParams: IMandatoryParams = defaultMandatoryParams
+) => {
   const lastQueryParams = useRef({});
   const location = useLocation();
 
   useEffect(() => {
     const componentQueryParamsObject = getComponentQueryParamsObject(location.search, componentId);
-    // Prevent service invocation when no Query Params changed
+
+    // Prevent service invocation:
     if (
+      // 1) when no Query Params changed
       Object.keys(componentQueryParamsObject).length &&
-      !queryParamsObjectsAreEqual(lastQueryParams.current, componentQueryParamsObject)
+      !queryParamsObjectsAreEqual(lastQueryParams.current, componentQueryParamsObject) &&
+      // 2) until all mandatory Query Parameters are available in the URL
+      areMandatoryParamsAvailable(mandatoryParams, componentQueryParamsObject)
     ) {
       lastQueryParams.current = componentQueryParamsObject;
       // Put Query Params coming from the URL to the service
       service({ params: componentQueryParamsObject });
     }
-  }, [location.search, componentId, service]);
+  }, [location.search, componentId, service, mandatoryParams]);
 };
