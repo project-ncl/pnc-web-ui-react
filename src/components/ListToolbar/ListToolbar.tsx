79db6c93941bc7353be95a303a4830cd07d775ca
Patrik Korytar <patrik.korytar@gmail.com>
2022-10-12 16:30:44 +0200

    NCL-6963 Seperate flex config object from component

--- src/components/ListToolbar/ListToolbar.tsx
+++ src/components/ListToolbar/ListToolbar.tsx
@@ -1,11 +1,11 @@
 import { Card, CardBody } from '@patternfly/react-core';
 
-const ListToolbarStyle = { marginBottom: '15px' };
+const listToolbarStyle = { marginBottom: '15px' };
 
 interface IListToolbarProps {}
 
 export const ListToolbar = ({ children }: React.PropsWithChildren<IListToolbarProps>) => (
-  <Card style={ListToolbarStyle}>
+  <Card style={listToolbarStyle}>
     <CardBody>{children}</CardBody>
   </Card>
 );
