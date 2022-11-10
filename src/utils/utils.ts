43ca9d4883bcf8b0d3f8aadb8ac0b7608b7a38f5
Dennis Zhou <tozhou@redhat.com>
2022-11-10 02:08:12 +0000

    [NCL-7388] Extract date util function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,3 +19,16 @@ export const createDateTime = ({ date, includeDate = true, includeTime = true }:
     ...(includeDate && { dateStyle: 'medium' }),
     ...(includeTime && { timeStyle: 'medium' }),
   }).format(typeof date === 'string' ? new Date(date) : date);
+
+export const transDateFormat = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const monthString = month < 10 ? `0${month}` : month;
+  const dayString = day < 10 ? `0${day}` : day;
+  const hourString = hour < 10 ? `0${hour}` : hour;
+  const minuteString = minute < 10 ? `0${minute}` : minute;
+  return `${year}-${monthString}-${dayString} ${hourString}:${minuteString}`;
+};
