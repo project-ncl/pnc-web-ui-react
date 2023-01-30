26661bb1097d42c02f8ca647e30ad88fac59eda3
Patrik Korytar <patrik.korytar@gmail.com>
2023-01-30 14:27:50 +0100

    NCL-7574 Implement inline styles for ServiceContainerLoading

--- src/components/EmptyStates/ErrorStateCard.tsx
+++ src/components/EmptyStates/ErrorStateCard.tsx
@@ -1,10 +1,13 @@
+import { Icon } from '@patternfly/react-core';
 import { CubesIcon } from '@patternfly/react-icons';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
 
 import { StateCard } from './StateCard';
 
 interface IErrorStateCard {
   title: string;
   error?: string;
+  isInline: boolean;
 }
 
 /**
@@ -13,8 +16,23 @@ interface IErrorStateCard {
  * @param title - Title subject, for example "Project List"
  * @param error - Error details
  */
-export const ErrorStateCard = ({ title, error }: IErrorStateCard) => (
-  <StateCard title={`Error when loading ${title}`} icon={CubesIcon}>
-    <pre>{error}</pre>
-  </StateCard>
-);
+export const ErrorStateCard = ({ title, error, isInline }: IErrorStateCard) => {
+  const errorTitle = `Error when loading ${title}`;
+
+  if (isInline) {
+    return (
+      <span>
+        <Icon status="danger">
+          <ExclamationCircleIcon />
+        </Icon>
+        {` ${errorTitle}`}
+      </span>
+    );
+  }
+
+  return (
+    <StateCard title={errorTitle} icon={CubesIcon}>
+      <pre>{error}</pre>
+    </StateCard>
+  );
+};
