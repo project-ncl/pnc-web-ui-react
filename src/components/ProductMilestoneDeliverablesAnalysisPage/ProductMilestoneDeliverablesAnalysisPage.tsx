d5968ce966a8e70bdc9a0a6b4f8a4a4146f3c166
Patrik Korytar <patrik.korytar@gmail.com>
2023-02-16 15:55:27 +0100

    NCL-7224 Implement Product Milestone tabs structure

---
+++ src/components/ProductMilestoneDeliverablesAnalysisPage/ProductMilestoneDeliverablesAnalysisPage.tsx
@@ -0,0 +1,11 @@
+import { PageSection } from '@patternfly/react-core';
+
+import { ContentBox } from 'components/ContentBox/ContentBox';
+
+export const ProductMilestoneDeliverablesAnalysisPage = () => {
+  return (
+    <PageSection>
+      <ContentBox padding marginBottom></ContentBox>
+    </PageSection>
+  );
+};
---
+++ src/components/ProductMilestoneDeliveredArtifactsPage/ProductMilestoneDeliveredArtifactsPage.tsx
@@ -0,0 +1,11 @@
+import { PageSection } from '@patternfly/react-core';
+
+import { ContentBox } from 'components/ContentBox/ContentBox';
+
+export const ProductMilestoneDeliveredArtifactsPage = () => {
+  return (
+    <PageSection>
+      <ContentBox padding marginBottom></ContentBox>
+    </PageSection>
+  );
+};
