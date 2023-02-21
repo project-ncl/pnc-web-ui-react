e888a22489a6d1f7ae562f62d19e0296b1b04c12
Martin Kelnar <mkelnar@redhat.com>
2023-02-21 13:19:54 +0100

    NCL-7392 Clean deprecated Sorting component

--- src/hooks/useSorting.ts
+++ src/hooks/useSorting.ts
@@ -2,10 +2,82 @@ import { ISortBy, ThProps } from '@patternfly/react-table';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { ISortOptions } from 'components/Sorting/Sorting';
-
 import { getComponentQueryParamValue, updateQueryParamsInURL } from 'utils/queryParamsHelper';
 import { validateSortParam } from 'utils/sortParamHelper';
+
+/**
+ * @example
+ * {
+ *   id: 'name',
+ *   title: 'Name',
+ *   tableColumnIndex: 0
+ * }
+ *
+ * @example
+ * {
+ *   id: 'description',
+ *   title: 'Description',
+ *   isDefault: true,
+ *   tableColumnIndex: 1
+ * }
+ *
+ * @example
+ * {
+ *   id: 'name',
+ *   title: 'Name',
+ *   isDefault: true,
+ *   defaultSortOrder: 'asc',
+ *   tableColumnIndex: 0
+ * }
+ *
+ * @example
+ * {
+ *   id: 'description',
+ *   title: 'Description',
+ *   isDefault: true,
+ *   defaultSortOrder: 'desc',
+ *   tableColumnIndex: 1
+ * }
+ */
+interface ISortAttribute {
+  /**
+   * ID has to match object key {@link ISortOptions}, there is automatic checker throwing errors if they don't match.
+   */
+  id: string;
+  /**
+   * Sorting option title.
+   */
+  title: string;
+  /**
+   * Should be sorted by default by this attribute?
+   * Maximally one attribute should be default.
+   */
+  isDefault?: boolean;
+  /**
+   * If sorted by default by this attribute, which order is used?
+   * If omitted, ascending order is assumed.
+   */
+  defaultSortOrder?: ISortBy['direction'];
+
+  /**
+   * PatternFly requires to use table column indexes for sorting purposes.
+   * See https://www.patternfly.org/v4/components/table/
+   *
+   * First column = 0, the second column = 1, etc.
+   */
+  tableColumnIndex: number;
+}
+
+/**
+ * @example
+ * {
+ *   name: {ISortAttribute},
+ *   description: {ISortAttribute}
+ * }
+ */
+export interface ISortOptions {
+  [key: string]: ISortAttribute;
+}
 
 /**
  * Hook managing sorting functionality for list components like {@link ProjectsList}.
@@ -17,6 +89,29 @@ import { validateSortParam } from 'utils/sortParamHelper';
  * @param componentId - Component ID
  */
 export const useSorting = (sortOptions: ISortOptions, componentId: string) => {
+  /**
+   * Check sort options validity. In the future this could be replaced by unified solution covering filtering and sorting.
+   */
+  const sortOptionChecker = () => {
+    const sortOptionsArray = Object.entries(sortOptions);
+
+    const defaultSortOptionsArray = sortOptionsArray.filter(([_, v]) => v.isDefault);
+    if (defaultSortOptionsArray.length > 1) {
+      const defaultSortOptionsKeysString = defaultSortOptionsArray.map((arr) => arr[0]).join(', ');
+      // #log
+      console.warn('Sorting: More than one sorting options were specified:', defaultSortOptionsKeysString);
+    }
+
+    sortOptionsArray.forEach(([k, v]) => {
+      if (k !== v.id) {
+        // #log
+        throw new Error(`sortOptions have invalid format, object key (${k}) has to match id field (${v.id})!`);
+      }
+    });
+  };
+
+  sortOptionChecker();
+
   const location = useLocation();
   const navigate = useNavigate();
 
