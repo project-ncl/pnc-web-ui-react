b1751c405a2214515cbdba00b955da0e83e7947f
Patrik Korytar <patrik.korytar@gmail.com>
2023-02-21 15:20:35 +0100

    NCL-7604 Change global page title format

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 
+import { PageTitles } from 'common/constants';
+
 export const useTitle = (title: string) => {
   useEffect(() => {
-    document.title = title + ' | Newcastle Build System';
+    document.title = title + ` ${PageTitles.delimiterSymbol} PNC`;
   }, [title]);
 };
