4d62b0367615c9e5ff59749c7e6d3b7769398145
Patrik Korytar <patrik.korytar@gmail.com>
2023-02-22 16:13:45 +0100

    NCL-7596 Implement tabs using Patternfly html variant

--- src/components/Tabs/TabItem.tsx
+++ src/components/Tabs/TabItem.tsx
@@ -1,6 +1,7 @@
-import { Tab as TabPF, TabTitleText } from '@patternfly/react-core';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/react-styles/css/components/Tabs/tabs';
 import { PropsWithChildren } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface ITabItem {
   url: string;
@@ -9,6 +10,13 @@ interface ITabItem {
 
 export const TabItem = ({ children, url, index }: PropsWithChildren<ITabItem>) => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  return <TabPF title={<TabTitleText>{children}</TabTitleText>} onClick={() => navigate(url)} eventKey={index || 0} />;
+  return (
+    <li className={css(styles.tabsItem, pathname.includes(url) && styles.modifiers.current)}>
+      <button onClick={() => navigate(url)} type="button" className={styles.tabsLink}>
+        <span className={styles.tabsItemText}>{children}</span>
+      </button>
+    </li>
+  );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,8 +1,7 @@
-import { PageSection, PageSectionVariants, Tabs as TabsPF } from '@patternfly/react-core';
-import { Children, ReactElement, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
-import { TabItem } from 'components/Tabs/TabItem';
+import { PageSection, PageSectionVariants } from '@patternfly/react-core';
+import { css } from '@patternfly/react-styles';
+import stylesPF from '@patternfly/react-styles/css/components/Tabs/tabs';
+import { ReactElement } from 'react';
 
 import styles from './Tabs.module.css';
 
@@ -11,26 +10,16 @@ interface ITabs {
 }
 
 export const Tabs = ({ children }: ITabs) => {
-  const [activeTabKey, setActiveTabKey] = useState(0);
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    Children.forEach(children, (child, index) => {
-      if (pathname.includes(child.props.url)) {
-        setActiveTabKey(index);
-      }
-    });
-  }, [children, pathname]);
-
   return (
-    <PageSection className={styles['page-section-tabs']} type="tabs" variant={PageSectionVariants.light}>
-      <TabsPF activeKey={activeTabKey} usePageInsets>
-        {Children.map(children, (child, index) => (
-          <TabItem url={child.props.url} index={index}>
-            {child.props.children}
-          </TabItem>
-        ))}
-      </TabsPF>
+    <PageSection
+      stickyOnBreakpoint={{ default: 'top' }}
+      className={styles['page-section-tabs']}
+      type="tabs"
+      variant={PageSectionVariants.light}
+    >
+      <div className={css(stylesPF.tabs, stylesPF.modifiers.pageInsets)}>
+        <ul className={stylesPF.tabsList}>{children}</ul>
+      </div>
     </PageSection>
   );
 };
