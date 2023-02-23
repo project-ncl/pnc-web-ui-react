14dcc45c77f61071713289613e5743d74871a940
Patrik Korytar <patrik.korytar@gmail.com>
2023-02-23 10:48:41 +0100

    NCL-7596 Fix ProductsPage test

--- src/components/ProductsPage/__tests__/ProductsPage.test.tsx
+++ src/components/ProductsPage/__tests__/ProductsPage.test.tsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { ProductsPage } from 'components/ProductsPage/ProductsPage';
 
 test('renders ProductsPage', () => {
-  render(<ProductsPage />);
+  render(
+    <MemoryRouter>
+      <ProductsPage />
+    </MemoryRouter>
+  );
 });
