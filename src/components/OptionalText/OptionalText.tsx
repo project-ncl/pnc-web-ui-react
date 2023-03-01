69c3028fd3758382ca7d2782631a251677baef5b
Patrik Korytar <patrik.korytar@gmail.com>
2023-03-01 16:54:06 +0100

    NCL-7224 Delete OptionalText component

--- src/components/OptionalText/OptionalText.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-interface IOptionalTextProps {}
-
-export const OptionalText = ({ children }: PropsWithChildren<IOptionalTextProps>) => {
-  return children ? <>{children}</> : <>&mdash;</>;
-};
