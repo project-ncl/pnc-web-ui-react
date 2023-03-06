3fa964e5f0c02d211291efde532595bf2f99f14e
Dennis Zhou <tozhou@redhat.com>
2023-03-06 01:08:35 +0000

    [NCL-7200] Add clipboard and tooltip components

---
+++ src/components/CopyToClipboard/__tests__/CopyToClipboard.text.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { CopyToClipboard } from 'components/CopyToClipboard/CopyToClipboard';
+
+const TEST_CONTENT = 'http://testUrl.com';
+
+test('renders CopyToClipboard', () => {
+  render(<CopyToClipboard>{TEST_CONTENT}</CopyToClipboard>);
+});
+
+test('compare snapshot with previous record', () => {
+  const tree = render(<CopyToClipboard>{TEST_CONTENT}</CopyToClipboard>);
+  expect(tree).toMatchSnapshot();
+});
