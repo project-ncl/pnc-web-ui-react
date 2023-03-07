1e5bf296b799bb849d4e1d50dccd46e6753af461
Dennis Zhou <tozhou@redhat.com>
2023-03-07 02:11:14 +0000

    [NCL-7200] Remove unused import

--- src/components/ScmRepositoryLink/__tests__/ScmRepositoryLink.test.tsx
+++ src/components/ScmRepositoryLink/__tests__/ScmRepositoryLink.test.tsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 
 import { ScmRepositoryLink } from 'components/ScmRepositoryLink/ScmRepositoryLink';
 
