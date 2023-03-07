0f977bb9215264be813abc119b3c465deb4fa60b
Patrik Korytar <patrik.korytar@gmail.com>
2023-03-07 14:25:43 +0100

    NCL-7632 Refactor ParsedArtifactIdentifier component

--- src/components/ParsedArtifactIdentifier/ParsedArtifactIdentifier.tsx
+++ src/components/ParsedArtifactIdentifier/ParsedArtifactIdentifier.tsx
@@ -1,21 +1,24 @@
-import { Flex, FlexItem, FlexProps, Label } from '@patternfly/react-core';
+import { Flex, FlexItem, FlexProps, Label, LabelProps } from '@patternfly/react-core';
 
 import { Artifact } from 'pnc-api-types-ts';
 
-const generateLastLabel = (artifactNameSplitted: string[], lastIndex: number) => {
-  return (
-    artifactNameSplitted.length > lastIndex && (
-      <>
-        <FlexItem>:</FlexItem>
-        <FlexItem>
-          <Label variant="outline" color="grey" isCompact>
-            {artifactNameSplitted.slice(lastIndex).join(':')}
-          </Label>
-        </FlexItem>
-      </>
-    )
-  );
-};
+const LABEL_COLORS: LabelProps['color'][] = ['orange', 'cyan', 'purple', 'green', 'grey'];
+
+interface IGeneratedLabelProps {
+  artifactLabel: string;
+  index: number;
+}
+
+const GeneratedLabel = ({ artifactLabel, index }: IGeneratedLabelProps) => (
+  <>
+    {index !== 0 && <FlexItem>:</FlexItem>}
+    <FlexItem>
+      <Label variant="outline" color={LABEL_COLORS[index]} isCompact>
+        {artifactLabel}
+      </Label>
+    </FlexItem>
+  </>
+);
 
 const spaceItemsXs: FlexProps['spaceItems'] = { default: 'spaceItemsXs' };
 
@@ -23,56 +26,28 @@ interface IParsedArtifactIdentifierProps {
   artifact: Artifact;
 }
 
+/**
+ * Component to highlight labels of Artifact identifier.
+ * Identifier is PNC specific identifier only derived from the GAV, etc.
+ *
+ * @param artifact - Artifact (containing identifier)
+ */
 export const ParsedArtifactIdentifier = ({ artifact }: IParsedArtifactIdentifierProps) => {
   const artifactNameSplitted = artifact.identifier.split(':');
   const repoType = artifact.targetRepository?.repositoryType;
 
-  if (repoType === 'MAVEN') {
-    return (
-      <Flex spaceItems={spaceItemsXs}>
-        <FlexItem>
-          <Label variant="outline" color="orange" isCompact>
-            {artifactNameSplitted[0]}
-          </Label>
-        </FlexItem>
-        <FlexItem>:</FlexItem>
-        <FlexItem>
-          <Label variant="outline" color="cyan" isCompact>
-            {artifactNameSplitted[1]}
-          </Label>
-        </FlexItem>
-        <FlexItem>:</FlexItem>
-        <FlexItem>
-          <Label variant="outline" color="purple" isCompact>
-            {artifactNameSplitted[2]}
-          </Label>
-        </FlexItem>
-        <FlexItem>:</FlexItem>
-        <FlexItem>
-          <Label variant="outline" color="green" isCompact>
-            {artifactNameSplitted[3]}
-          </Label>
-        </FlexItem>
-        {generateLastLabel(artifactNameSplitted, 4)}
-      </Flex>
-    );
-  }
+  if (repoType === 'MAVEN' || repoType === 'NPM') {
+    const labelCount = repoType === 'MAVEN' ? 4 : 2;
+    const artifactLabels = [
+      ...artifactNameSplitted.slice(0, labelCount),
+      ...(artifactNameSplitted.length > labelCount ? [artifactNameSplitted.slice(labelCount).join(':')] : []),
+    ];
 
-  if (repoType === 'NPM') {
     return (
       <Flex spaceItems={spaceItemsXs}>
-        <FlexItem>
-          <Label variant="outline" color="orange" isCompact>
-            {artifactNameSplitted[0]}
-          </Label>
-        </FlexItem>
-        <FlexItem>:</FlexItem>
-        <FlexItem>
-          <Label variant="outline" color="cyan" isCompact>
-            {artifactNameSplitted[1]}
-          </Label>
-        </FlexItem>
-        {generateLastLabel(artifactNameSplitted, 2)}
+        {artifactLabels.map((artifactLabel: string, index: number) => (
+          <GeneratedLabel artifactLabel={artifactLabel} index={index} />
+        ))}
       </Flex>
     );
   }
