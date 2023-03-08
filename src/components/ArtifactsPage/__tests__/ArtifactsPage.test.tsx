59b8f87318743b09be69228c5144d8e1c8d84c1d
Patrik Korytar <patrik.korytar@gmail.com>
2023-03-08 16:06:04 +0100

    NCL-7218 Add snapshot test for ArtifactsPage

--- src/components/ArtifactsPage/__tests__/ArtifactsPage.test.tsx
+++ src/components/ArtifactsPage/__tests__/ArtifactsPage.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { ArtifactsPage } from 'components/ArtifactsPage/ArtifactsPage';
@@ -7,10 +7,35 @@ jest.mock('services/artifactApi');
 jest.mock('services/keycloakService');
 jest.mock('services/uiLogger');
 
-test('renders ArtifactsPage', () => {
-  render(
-    <MemoryRouter>
-      <ArtifactsPage />
-    </MemoryRouter>
-  );
+describe('display ArtifactsPage component', () => {
+  let artifactsMock: any;
+
+  async function loadMocks() {
+    const artifactsRequestMock = await import('services/__mocks__/artifacts-mock.json');
+    artifactsMock = artifactsRequestMock.content;
+  }
+
+  beforeEach(async () => {
+    await loadMocks();
+  });
+
+  test('renders ArtifactsPage', () => {
+    render(
+      <MemoryRouter>
+        <ArtifactsPage />
+      </MemoryRouter>
+    );
+  });
+
+  test('compare snapshot with previous record', async () => {
+    let tree: any;
+    await act(async () => {
+      tree = render(
+        <MemoryRouter>
+          <ArtifactsPage />
+        </MemoryRouter>
+      );
+    });
+    expect(tree).toMatchSnapshot();
+  });
 });
