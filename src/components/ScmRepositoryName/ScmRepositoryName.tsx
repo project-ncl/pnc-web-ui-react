b17cd79473bf5660eebfafb1eadff3ec781080a0
Dennis Zhou <tozhou@redhat.com>
2023-03-10 13:20:48 +0000

    NCL-7199 Update to use internalUrl for ScmRepositoryName

--- src/components/ScmRepositoryName/ScmRepositoryName.tsx
+++ src/components/ScmRepositoryName/ScmRepositoryName.tsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 import { parseScmRepositoryTitle } from 'utils/utils';
 
 interface IScmRepositoryNameProps {
-  url: string;
+  internalUrl: string;
   scmRepositoryId: string;
 }
 
 /**
  * The SCM Repository name that lead to SCM Repository detail page.
  *
- * @param url - the internal/external url for the SCM Repository
+ * @param internalUrl - the internal url for the SCM Repository
  * @param scmRepositoryId - id of the SCM Repository,
  */
-export const ScmRepositoryName = ({ url, scmRepositoryId }: IScmRepositoryNameProps) => {
-  return <Link to={scmRepositoryId}>{parseScmRepositoryTitle({ internalUrl: url })}</Link>;
+export const ScmRepositoryName = ({ internalUrl, scmRepositoryId }: IScmRepositoryNameProps) => {
+  return <Link to={scmRepositoryId}>{parseScmRepositoryTitle({ internalUrl })}</Link>;
 };
