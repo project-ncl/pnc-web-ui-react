60fe998289843617226b1c02578d15bb18722f9d
Patrik Korytar <patrik.korytar@gmail.com>
2023-03-14 12:42:30 +0100

    NCL-7622 Enhance EmptyStateSymbol component

--- src/components/EmptyStates/EmptyStateSymbol.tsx
+++ src/components/EmptyStates/EmptyStateSymbol.tsx
@@ -1,4 +1,14 @@
+import styles from './EmptyStateSymbol.module.css';
+
 interface IEmptyStateSymbol {
   title?: string;
+  variant?: 'text' | 'mdash' | 'ndash';
 }
-export const EmptyStateSymbol = ({ title }: IEmptyStateSymbol) => <span title={`${title} is not available`}>&ndash;</span>;
+
+export const EmptyStateSymbol = ({ title, variant = 'mdash' }: IEmptyStateSymbol) => (
+  <span title={`${title} is not available`}>
+    {variant === 'text' && <span className={styles['value-empty']}>Empty</span>}
+    {variant === 'mdash' && <>&mdash;</>}
+    {variant === 'ndash' && <>&ndash;</>}
+  </span>
+);
