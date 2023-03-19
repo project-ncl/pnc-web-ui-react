59f02e6c5433ac2cd503d57cf5a051bdd0e8e0b3
Dennis Zhou <tozhou@redhat.com>
2023-03-19 20:28:57 +0000

    NCL-7199 Fix entity name generator bug

--- src/utils/entityNameGenerators.ts
+++ src/utils/entityNameGenerators.ts
@@ -11,4 +11,4 @@ interface IGenerateScmRepositoryName {
  * @returns {string} SCM Repository name
  */
 export const generateScmRepositoryName = ({ scmRepository }: IGenerateScmRepositoryName): string =>
-  scmRepository!.internalUrl ? scmRepository.internalUrl.split('/').splice(3).join('/') : '';
+  scmRepository && scmRepository.internalUrl ? scmRepository.internalUrl.split('/').splice(3).join('/') : '';
