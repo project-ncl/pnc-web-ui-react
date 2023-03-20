f1aae06b64b7f47aa56c80518d0d58daf7ec297c
Dennis Zhou <tozhou@redhat.com>
2023-03-20 17:54:11 +0000

    NCL-7199 Extract Create Repository button to PageLayout level

--- src/components/ScmRepositoriesPage/ScmRepositoriesPage.tsx
+++ src/components/ScmRepositoriesPage/ScmRepositoriesPage.tsx
@@ -6,7 +6,9 @@ import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 import { useTitle } from 'hooks/useTitle';
 
+import { ActionButton } from 'components/ActionButton/ActionButton';
 import { PageLayout } from 'components/PageLayout/PageLayout';
+import { ProtectedComponent } from 'components/ProtectedContent/ProtectedComponent';
 import { ScmRepositoriesList } from 'components/ScmRepositoriesList/ScmRepositoriesList';
 
 import * as scmRepositoryApi from 'services/scmRepositoryApi';
@@ -29,6 +31,11 @@ export const ScmRepositoriesPage = ({ componentId = 's1' }: IScmRepositoriesPage
           This page contains SCM Repositories like <Label>apache/maven.git</Label> or <Label>git/twitter4j.git</Label>, they are
           either created by new build config wizard or manually created here.
         </>
+      }
+      actions={
+        <ProtectedComponent>
+          <ActionButton action={() => console.log('Not implemented yet!')}>Create SCM Repository</ActionButton>
+        </ProtectedComponent>
       }
     >
       <ScmRepositoriesList {...{ serviceContainerScmRepositories, componentId }} />
