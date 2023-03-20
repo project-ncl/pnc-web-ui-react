0137c06f149903d434874fed82d86a9e2f7a4cf2
Dennis Zhou <tozhou@redhat.com>
2023-03-20 01:05:08 +0000

    NCL-7649 Update snapshots for unit tests

--- src/components/ScmRepositoryUrl/__tests__/ScmRepositoryUrl.test.tsx
+++ src/components/ScmRepositoryUrl/__tests__/ScmRepositoryUrl.test.tsx
@@ -4,11 +4,31 @@ import { ScmRepositoryUrl } from 'components/ScmRepositoryUrl/ScmRepositoryUrl';
 
 describe('display ScmRepositoryUrl component', () => {
   test('renders ScmRepositoryLink', () => {
-    render(<ScmRepositoryUrl url="https://test.url.com/project.git" />);
+    render(
+      <ScmRepositoryUrl
+        isInternal
+        scmRepository={{
+          id: '103',
+          internalUrl: 'git+ssh://code.test.env.com/testRepo/testUrlClipboardCopyGerrit.git',
+          externalUrl: 'https://github.com/testRepo/empty.git',
+          preBuildSyncEnabled: true,
+        }}
+      />
+    );
   });
 
   test('compare snapshot with previous record', () => {
-    const tree = render(<ScmRepositoryUrl url="https://test.url.com/project.git" />);
+    const tree = render(
+      <ScmRepositoryUrl
+        isInternal
+        scmRepository={{
+          id: '103',
+          internalUrl: 'git+ssh://code.test.env.com/testRepo/testUrlClipboardCopyGerrit.git',
+          externalUrl: 'https://github.com/testRepo/empty.git',
+          preBuildSyncEnabled: true,
+        }}
+      />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
