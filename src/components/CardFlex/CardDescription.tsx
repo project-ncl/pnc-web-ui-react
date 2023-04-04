dff4934f83abd4920f31ac7305fe19b6f9135a96
Patrik Korytar <patrik.korytar@gmail.com>
2023-04-04 15:31:21 +0200

    NCL-7628 Rename CardDescription to CardTitle

--- src/components/CardFlex/CardDescription.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import styles from './CardDescription.module.css';
-
-interface ICardDescriptionProps {}
-
-export const CardDescription = ({ children }: PropsWithChildren<ICardDescriptionProps>) => (
-  <div className={styles['card-description']}>{children}</div>
-);
