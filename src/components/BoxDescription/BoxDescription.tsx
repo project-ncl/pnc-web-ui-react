0596ecdabe5ed3d962cc452d4d0b66e2ee688b96
Patrik Korytar <patrik.korytar@gmail.com>
2023-04-04 16:00:46 +0200

    NCL-7628 Change BoxDescription tsdoc

--- src/components/BoxDescription/BoxDescription.tsx
+++ src/components/BoxDescription/BoxDescription.tsx
@@ -19,11 +19,12 @@ interface IBoxDescriptionProps {
 }
 
 /**
- * Icon with description displayed on hover event.
+ * Description for box components, for example @link ContentBox, displayed either within tooltip or popover.
  *
- * Of description is string, tooltip is used, popover otherwise.
+ * If description is string, tooltip is used.
+ * If description is ReactNode, popover is used.
  *
- * @param description - Description to be displayed on icon hover
+ * @param description - Description to be displayed within tooltip / popover
  */
 export const BoxDescription = ({ description }: IBoxDescriptionProps) => (
   <div className={styles['box-description']}>
