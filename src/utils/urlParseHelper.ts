e92ae90b8b56dbff55feb322df997dd3e598252b
Dennis Zhou <tozhou@redhat.com>
2023-04-10 23:15:15 +0100

    NCL-7649 Refactor IParsedUrl interface

--- src/utils/urlParseHelper.ts
+++ src/utils/urlParseHelper.ts
@@ -3,7 +3,8 @@ interface IScmRepositoryUrl {
 }
 
 export interface IParsedUrl {
-  url: string;
+  webUrl: string;
+  scmRepositoryUrl: string;
   name: string;
 }
 
@@ -11,34 +12,38 @@ export interface IParsedUrl {
  * Parses internal SCM Repository URL to Gerrit gitweb link of the SCM Repository.
  *
  * @param url - The internalUrl to be parsed
- * @returns Object containing parsed URL and display name representing URL
+ * @returns Object containing scmRepository URL, parsed URL and display name representing URL
  *  */
 export const parseInternalScmRepositoryUrl = ({ url }: IScmRepositoryUrl): IParsedUrl => {
   const protocol = url.split('://')[0];
   const base = url.split('://')[1].split('/')[0];
   const project = url.split(base + (['https', 'http'].includes(protocol) ? '/gerrit/' : '/'))[1];
-  return { url: 'https://' + base + '/gerrit/gitweb?p=' + project + ';a=summary', name: 'Gerrit' };
+  return { scmRepositoryUrl: url, webUrl: 'https://' + base + '/gerrit/gitweb?p=' + project + ';a=summary', name: 'Gerrit' };
 };
 
 /**
  * ParsesSCM Repository URL to gitweb link of the SCM Repository.
  *
  * @param url - The externalUrl to be parsed
- * @returns  Object containing parsed URL and display name representing URL
+ * @returns  1) null when url was not successfully parsed, otherwise
+ * 2) object containing parsed URL and display name representing URL
  */
 export const parseExternalScmRepositoryUrl = ({ url }: IScmRepositoryUrl): IParsedUrl | null => {
+  if (!url) {
+    return null;
+  }
   if (url.includes('/gerrit/')) {
     return parseInternalScmRepositoryUrl({ url });
   }
   if (['http', 'https', '@'].some((element) => url.includes(element))) {
     const urlRes = url.includes('@') ? 'https://' + url.split('@')[1].replace(':', '/') : url;
     const base = urlRes.split('://')[1].split('/')[0];
-    return { url: urlRes, name: base };
+    return { scmRepositoryUrl: url, webUrl: urlRes, name: base };
   }
   if (url.includes('.git')) {
     const urlRes = url;
     const base = urlRes.split('/')[0];
-    return { url: urlRes, name: base };
+    return { scmRepositoryUrl: url, webUrl: urlRes, name: base };
   }
   return null;
 };
