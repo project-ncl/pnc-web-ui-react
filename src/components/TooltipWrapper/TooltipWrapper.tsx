8c8068bee98b4f6a1ed0102f4cf583d6513de25d
Patrik Korytar <patrik.korytar@gmail.com>
2023-04-18 13:27:58 +0200

    NCL-7768 Enhance empty tooltip state

--- src/components/TooltipWrapper/TooltipWrapper.tsx
+++ src/components/TooltipWrapper/TooltipWrapper.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from '@patternfly/react-core';
 import { InfoCircleIcon } from '@patternfly/react-icons';
 
 interface ITooltipWrapperProps {
-  tooltip: string;
+  tooltip?: string;
 }
 /**
  * Tooltip Wrapper to make any component (like icon) to become a tooltip component.
@@ -10,8 +10,11 @@ interface ITooltipWrapperProps {
  * @param tooltip - the tooltip to be displayed
  * @param children - the tooltip handler, by default <InfoCircleIcon />
  */
-export const TooltipWrapper = ({ tooltip, children }: React.PropsWithChildren<ITooltipWrapperProps>) => (
-  <Tooltip removeFindDomNode content={tooltip}>
-    <> {children ? <>{children}</> : <InfoCircleIcon />}</>
-  </Tooltip>
-);
+export const TooltipWrapper = ({ tooltip, children }: React.PropsWithChildren<ITooltipWrapperProps>) =>
+  tooltip ? (
+    <Tooltip removeFindDomNode content={tooltip}>
+      <> {children ? <>{children}</> : <InfoCircleIcon />}</>
+    </Tooltip>
+  ) : (
+    <> {children ? <>{children}</> : null}</>
+  );
