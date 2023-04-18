ec52af3155ef44a724f9adba1600b1d163da2273
Patrik Korytar <patrik.korytar@gmail.com>
2023-04-18 16:24:15 +0200

    NCL-7676 Implement PageIconLink component

---
+++ src/components/PageIconLink/PageIconLink.tsx
@@ -0,0 +1,25 @@
+import { ArrowRightIcon } from '@patternfly/react-icons';
+import { Link } from 'react-router-dom';
+
+import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
+
+interface IPageIconLinkProps {
+  to: string;
+  text?: string;
+  tooltip?: string;
+}
+
+/**
+ * Icon with link to internal page.
+ *
+ * @param to - link to redirect to
+ * @param text - text to display next to icon
+ * @param tooltip - tooltip text
+ */
+export const PageIconLink = ({ to, text, tooltip }: IPageIconLinkProps) => (
+  <TooltipWrapper tooltip={tooltip as string}>
+    <Link to={to}>
+      {text} <ArrowRightIcon />
+    </Link>
+  </TooltipWrapper>
+);
