63c31b5bfa5c1bdac50617b936d1b4caefea6cbc
Dennis Zhou <tozhou@redhat.com>
2023-05-03 02:27:26 +0100

    NCL-7688 Refactor ToolbarItem to alow floated item

--- src/components/Toolbar/ToolbarItem.tsx
+++ src/components/Toolbar/ToolbarItem.tsx
@@ -1,14 +1,20 @@
 import { FlexItem, FlexProps } from '@patternfly/react-core';
 
 const flexAlignRight: FlexProps['align'] = { default: 'alignRight' };
+const flexAllowFloat: FlexProps['flex'] = { default: 'flex_1' };
 
 interface IToolbarItemProps {
   alignRight?: boolean;
   marginLeft?: string;
+  allowFloat?: boolean;
 }
 
-export const ToolbarItem = ({ children, alignRight, marginLeft }: React.PropsWithChildren<IToolbarItemProps>) => (
-  <FlexItem style={marginLeft ? { marginLeft } : undefined} align={alignRight ? flexAlignRight : undefined}>
+export const ToolbarItem = ({ children, alignRight, marginLeft, allowFloat }: React.PropsWithChildren<IToolbarItemProps>) => (
+  <FlexItem
+    flex={allowFloat ? flexAllowFloat : undefined}
+    style={marginLeft ? { marginLeft } : undefined}
+    align={alignRight ? flexAlignRight : undefined}
+  >
     {children}
   </FlexItem>
 );
