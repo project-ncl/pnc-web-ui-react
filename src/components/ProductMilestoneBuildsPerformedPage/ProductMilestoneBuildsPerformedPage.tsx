698433ce785266c14aa380eeb436c4f7b046c88e
Patrik Korytar <patrik.korytar@gmail.com>
2023-05-11 11:56:43 +0200

    NCL-7224 Implement Builds Performed page

--- src/components/ProductMilestoneBuildsPerformedPage/ProductMilestoneBuildsPerformedPage.tsx
+++ src/components/ProductMilestoneBuildsPerformedPage/ProductMilestoneBuildsPerformedPage.tsx
@@ -1,5 +1,26 @@
-import { ContentBox } from 'components/ContentBox/ContentBox';
+import { useParams } from 'react-router-dom';
 
-export const ProductMilestoneBuildsPerformedPage = () => {
-  return <ContentBox padding marginBottom></ContentBox>;
+import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
+import { useServiceContainer } from 'hooks/useServiceContainer';
+
+import { BuildsList } from 'components/BuildsList/BuildsList';
+
+import * as productMilestoneApi from 'services/productMilestoneApi';
+
+interface IProductMilestoneBuildsPerformedPageProps {
+  componentId?: string;
+}
+
+export const ProductMilestoneBuildsPerformedPage = ({ componentId = 'b1' }: IProductMilestoneBuildsPerformedPageProps) => {
+  const { productMilestoneId } = useParams();
+
+  const serviceContainerBuilds = useServiceContainer(productMilestoneApi.getBuilds);
+  const serviceContainerBuildsRunner = serviceContainerBuilds.run;
+
+  useQueryParamsEffect(
+    ({ requestConfig } = {}) => serviceContainerBuildsRunner({ serviceData: { id: productMilestoneId }, requestConfig }),
+    { componentId }
+  );
+
+  return <BuildsList {...{ serviceContainerBuilds, componentId }} />;
 };
