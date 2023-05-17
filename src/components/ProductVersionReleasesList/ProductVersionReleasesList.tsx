fa3b52498fa6bedee78689fb2a7714356b721d92
Patrik Korytar <patrik.korytar@gmail.com>
2023-05-17 11:28:23 +0200

    NCL-7223 Implement Product Releases list

---
+++ src/components/ProductVersionReleasesList/ProductVersionReleasesList.tsx
@@ -0,0 +1,114 @@
+import { ActionsColumn, TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+
+import { ProductRelease } from 'pnc-api-types-ts';
+
+import { PageTitles } from 'common/constants';
+
+import { IServiceContainer } from 'hooks/useServiceContainer';
+
+import { ContentBox } from 'components/ContentBox/ContentBox';
+import { Filtering, IFilterAttributes } from 'components/Filtering/Filtering';
+import { Pagination } from 'components/Pagination/Pagination';
+import { ProductMilestoneReleaseLabel } from 'components/ProductMilestoneReleaseLabel/ProductMilestoneReleaseLabel';
+import { useServiceContainerProductVersion } from 'components/ProductVersionPages/ProductVersionPages';
+import { ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
+import { Toolbar } from 'components/Toolbar/Toolbar';
+import { ToolbarItem } from 'components/Toolbar/ToolbarItem';
+
+import { createDateTime } from 'utils/utils';
+
+const filterAttributes: IFilterAttributes = {
+  filterAttributes: {
+    version: {
+      id: 'version',
+      title: 'Version',
+      operator: '=like=',
+    },
+    supportLevel: {
+      id: 'supportLevel',
+      title: 'Support Level',
+      filterValues: ['UNRELEASED', 'EARLYACCESS', 'SUPPORTED', 'EXTENDED_SUPPORT', 'EOL'],
+      operator: '==',
+    },
+  },
+};
+
+interface IProductVersionReleasesListProps {
+  serviceContainerProductReleases: IServiceContainer;
+  componentId: string;
+}
+
+/**
+ * Component displaying list of Product Releases.
+ *
+ * @param serviceContainerProductReleases - Service Container for Product Releases
+ * @param componentId - Component ID
+ */
+export const ProductVersionReleasesList = ({
+  serviceContainerProductReleases,
+  componentId,
+}: IProductVersionReleasesListProps) => {
+  const { serviceContainerProductVersion } = useServiceContainerProductVersion();
+
+  return (
+    <>
+      <Toolbar>
+        <ToolbarItem>
+          <Filtering filterAttributes={filterAttributes} componentId={componentId} />
+        </ToolbarItem>
+      </Toolbar>
+
+      <ContentBox borderTop>
+        <ServiceContainerLoading {...serviceContainerProductReleases} title={PageTitles.productReleases}>
+          <TableComposable isStriped variant="compact">
+            <Thead>
+              <Tr>
+                <Th width={25}>Version</Th>
+                <Th width={20}>Release Date</Th>
+                <Th width={20}>Released In</Th>
+                <Th width={20}>Support Level</Th>
+                <Th />
+              </Tr>
+            </Thead>
+            <Tbody>
+              {serviceContainerProductReleases.data?.content.map((productRelease: ProductRelease, rowIndex: number) => (
+                <Tr key={rowIndex}>
+                  <Td>
+                    <ProductMilestoneReleaseLabel productMilestoneRelease={productRelease} isCurrent={false} />
+                  </Td>
+                  <Td>{productRelease.releaseDate && createDateTime({ date: productRelease.releaseDate }).date}</Td>
+                  <Td>
+                    {productRelease.productMilestone && (
+                      <ProductMilestoneReleaseLabel
+                        link={`../milestones/${productRelease.productMilestone.id}`}
+                        productMilestoneRelease={productRelease.productMilestone}
+                        isCurrent={
+                          productRelease.productMilestone.id === serviceContainerProductVersion.data?.currentProductMilestone?.id
+                        }
+                      />
+                    )}
+                  </Td>
+                  <Td>{productRelease.supportLevel}</Td>
+                  <Td isActionCell>
+                    <ActionsColumn
+                      items={[
+                        {
+                          title: 'Edit Release',
+                        },
+                        {
+                          title: 'Delete Release',
+                        },
+                      ]}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </TableComposable>
+        </ServiceContainerLoading>
+      </ContentBox>
+
+      <Pagination componentId={componentId} count={serviceContainerProductReleases.data?.totalHits} />
+    </>
+  );
+};
--- src/components/ProductVersionReleasesPage/ProductVersionReleasesPage.tsx
+++ src/components/ProductVersionReleasesPage/ProductVersionReleasesPage.tsx
@@ -1,5 +1,26 @@
-import { ContentBox } from 'components/ContentBox/ContentBox';
+import { useParams } from 'react-router-dom';
 
-export const ProductVersionReleasesPage = () => {
-  return <ContentBox padding></ContentBox>;
+import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
+import { useServiceContainer } from 'hooks/useServiceContainer';
+
+import { ProductVersionReleasesList } from 'components/ProductVersionReleasesList/ProductVersionReleasesList';
+
+import * as productVersionApi from 'services/productVersionApi';
+
+interface IProductVersionReleasesPageProps {
+  componentId?: string;
+}
+
+export const ProductVersionReleasesPage = ({ componentId = 'r1' }: IProductVersionReleasesPageProps) => {
+  const { productVersionId } = useParams();
+
+  const serviceContainerProductReleases = useServiceContainer(productVersionApi.getProductReleases);
+  const serviceContainerProductReleasesRunner = serviceContainerProductReleases.run;
+
+  useQueryParamsEffect(
+    ({ requestConfig } = {}) => serviceContainerProductReleasesRunner({ serviceData: { id: productVersionId }, requestConfig }),
+    { componentId, mandatoryQueryParams: { pagination: true, sorting: false } }
+  );
+
+  return <ProductVersionReleasesList {...{ serviceContainerProductReleases, componentId }} />;
 };
