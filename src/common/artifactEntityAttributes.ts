76fb36a39155315823c489f705831ddf9b296ff1
Martin Kelnar <mkelnar@redhat.com>
2023-05-27 10:53:19 +0200

    NCL-7846 Create artifactEntityAttributes

---
+++ src/common/artifactEntityAttributes.ts
@@ -0,0 +1,32 @@
+export const artifactEntityAttributes = {
+  identifier: {
+    title: 'Identifier',
+  },
+  artifactQuality: {
+    title: 'Artifact Quality',
+  },
+  buildCategory: {
+    title: 'Build Category',
+  },
+  filename: {
+    title: 'Filename',
+  },
+  'targetRepository.repositoryType': {
+    title: 'Repository Type',
+  },
+  md5: {
+    title: 'md5',
+  },
+  sha1: {
+    title: 'sha1',
+  },
+  sha256: {
+    title: 'sha256',
+  },
+  'product.name': {
+    title: 'Product Name',
+  },
+  'productMilestone.version': {
+    title: 'Milestone Version',
+  },
+};
