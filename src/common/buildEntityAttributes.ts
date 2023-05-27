089bed16d3c25491bf93d2f06281e5982bff264f
Martin Kelnar <mkelnar@redhat.com>
2023-05-27 10:41:45 +0200

    NCL-7846 Create buildEntityAttributes

---
+++ src/common/buildEntityAttributes.ts
@@ -0,0 +1,29 @@
+export const buildEntityAttributes = {
+  id: {
+    title: 'ID',
+  },
+  buildConfigName: {
+    title: 'Build Config Name',
+  },
+  status: {
+    title: 'Status',
+  },
+  temporaryBuild: {
+    title: 'Temporary Build',
+  },
+  'user.username': {
+    title: 'User',
+  },
+  name: {
+    title: 'Name',
+  },
+  submitTime: {
+    title: 'Submitted',
+  },
+  startTime: {
+    title: 'Started',
+  },
+  endTime: {
+    title: 'Ended',
+  },
+};
