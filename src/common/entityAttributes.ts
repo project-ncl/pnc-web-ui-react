e3be4a1921a252765a263af720272fdac68a9a16
Martin Kelnar <mkelnar@redhat.com>
2023-06-26 06:32:41 +0200

    NCL-7848 Add IEntityAttribute and IEntityAttributes

--- src/common/entityAttributes.ts
+++ src/common/entityAttributes.ts
@@ -1,14 +1,20 @@
 import { IQParamOperators } from 'utils/qParamHelper';
 
-export type TEntityAttributes<Entity> = {
-  [key in keyof Entity]: {
-    id: key; // object key and id property value matches
-    title: string;
-    tooltip?: string;
-    values?: any;
-    filter?: {
-      operator: IQParamOperators;
-      isCustomParam?: boolean;
-    };
+export interface IEntityAttribute<T = string> {
+  id: T; // object key and id property value matches
+  title: string;
+  tooltip?: string;
+  values?: any;
+  filter?: {
+    operator: IQParamOperators;
+    isCustomParam?: boolean;
   };
+}
+
+export interface IEntityAttributes {
+  [key: string]: IEntityAttribute<typeof key>;
+}
+
+export type TEntityAttributes<Entity> = {
+  [key in keyof Entity]: IEntityAttribute<key>;
 };
