9697b1b626a3eeee34585339d542c9e3740ac408
Patrik Korytar <patrik.korytar@gmail.com>
2023-07-04 13:08:52 +0200

    NCL-7666 Add new padding options to Toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -7,10 +7,25 @@ const styleGap = { gap: '1.5rem' };
 
 interface IToolbarProps {
   borderTop?: boolean;
+  padding?: boolean;
+  disablePaddingTop?: boolean;
+  disablePaddingBottom?: boolean;
 }
 
-export const Toolbar = ({ children, borderTop = false }: React.PropsWithChildren<IToolbarProps>) => (
-  <ContentBox borderTop={borderTop} padding>
+export const Toolbar = ({
+  children,
+  borderTop,
+  padding = true,
+  disablePaddingTop,
+  disablePaddingBottom,
+}: React.PropsWithChildren<IToolbarProps>) => (
+  <ContentBox
+    borderTop={borderTop}
+    paddingTop={padding && !disablePaddingTop}
+    paddingBottom={padding && !disablePaddingBottom}
+    paddingLeft={padding}
+    paddingRight={padding}
+  >
     <Flex spaceItems={spaceItemsNone} style={styleGap}>
       {children}
     </Flex>
