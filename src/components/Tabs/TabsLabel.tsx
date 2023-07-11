e561872ecf96ae63112ac6b348260045086b887f
Dennis Zhou <tozhou@redhat.com>
2023-07-12 00:14:45 +0100

    NCL-7777 Fix content jumping issue

---
+++ src/components/Tabs/TabsLabel.tsx
@@ -0,0 +1,27 @@
+import { Label, Tooltip } from '@patternfly/react-core';
+import { PropsWithChildren } from 'react';
+
+import { IServiceContainer } from 'hooks/useServiceContainer';
+
+import { ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
+
+import styles from './TabsLabel.module.css';
+
+interface ITabsLabelProps {
+  serviceContainer: IServiceContainer;
+  title: string;
+}
+
+export const TabsLabel = ({ serviceContainer, title, children }: PropsWithChildren<ITabsLabelProps>) => {
+  return (
+    <span className={styles['page-section-tabs-label']}>
+      <Tooltip content={title}>
+        <Label>
+          <ServiceContainerLoading loadingDelayMilliseconds={0} {...serviceContainer} variant="inline" title={title}>
+            {children}
+          </ServiceContainerLoading>
+        </Label>
+      </Tooltip>
+    </span>
+  );
+};
