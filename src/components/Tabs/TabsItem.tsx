a9e0632058b5b994a45d6765757d164820b24fb8
Patrik Korytar <patrik.korytar@gmail.com>
2023-07-25 13:18:42 +0200

    NCL-8058 Make TabsItem component use Link component

--- src/components/Tabs/TabsItem.tsx
+++ src/components/Tabs/TabsItem.tsx
@@ -1,21 +1,20 @@
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Tabs/tabs';
 import { PropsWithChildren } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 interface ITabsItemProps {
   url: string;
 }
 
 export const TabsItem = ({ children, url }: PropsWithChildren<ITabsItemProps>) => {
-  const navigate = useNavigate();
   const { pathname } = useLocation();
 
   return (
     <li className={css(styles.tabsItem, pathname.includes(url) && styles.modifiers.current)}>
-      <button onClick={() => navigate(url)} type="button" className={styles.tabsLink}>
+      <Link to={url} className={styles.tabsLink}>
         <span className={styles.tabsItemText}>{children}</span>
-      </button>
+      </Link>
     </li>
   );
 };
