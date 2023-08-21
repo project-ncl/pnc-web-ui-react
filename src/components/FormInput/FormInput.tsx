840080c8c2f09ae738ca5ef0bfd82af5e758b4e4
Patrik Korytar <patrik.korytar@gmail.com>
2023-08-21 10:01:50 +0200

    NCL-7699 Implement form input wrapper component

---
+++ src/components/FormInput/FormInput.tsx
@@ -0,0 +1,21 @@
+import { IRegisterData } from 'hooks/useNewForm';
+
+interface IFormInputProps<T> extends IRegisterData<T> {
+  render: (registerData: IRegisterData<T>) => JSX.Element;
+}
+
+/**
+ * Form input wrapper component.
+ * It's meant to be used with the {@link useNewForm} hook when the hook's API is not compatible with the component's API.
+ * For example, PatternFly's Select component (onSelect) vs useForm hook (onChange).
+ *
+ * @param value - Value of the input
+ * @param onChange - On-change callback of the input
+ * @param onBlur - On-blur callback of the input
+ * @param validated - Input state
+ * @param render - Function to render the form input component
+ * @returns
+ */
+export const FormInput = <T,>({ value, onChange, onBlur, validated, render }: IFormInputProps<T>) => {
+  return render({ value, onChange, onBlur, validated });
+};
