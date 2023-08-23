2d3de44f686bd943b6de6fa36952a05262e76978
Patrik Korytar <patrik.korytar@gmail.com>
2023-08-23 12:57:28 +0200

    NCL-8144 Pass loadingStateDelay from useServiceContainer hook to ServiceContainerLoading component

--- src/components/Tabs/TabsLabel.tsx
+++ src/components/Tabs/TabsLabel.tsx
@@ -17,7 +17,7 @@ export const TabsLabel = ({ serviceContainer, title, children }: PropsWithChildr
     <span className={styles['page-section-tabs-label']}>
       <Tooltip content={title}>
         <Label>
-          <ServiceContainerLoading loadingStateDelay={0} {...serviceContainer} variant="inline" title={title}>
+          <ServiceContainerLoading {...serviceContainer} variant="inline" title={title}>
             {children}
           </ServiceContainerLoading>
         </Label>
