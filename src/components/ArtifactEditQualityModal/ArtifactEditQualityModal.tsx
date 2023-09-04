377309d54dc1a55c6a828373caefa92868d00b63
Patrik Korytar <patrik.korytar@gmail.com>
2023-09-04 11:45:03 +0200

    NCL-7699 Apply new form hook on Artifact quality edit page

--- src/components/ArtifactEditQualityModal/ArtifactEditQualityModal.tsx
+++ src/components/ArtifactEditQualityModal/ArtifactEditQualityModal.tsx
@@ -7,10 +7,11 @@ import { Artifact } from 'pnc-api-types-ts';
 import { artifactEntityAttributes } from 'common/artifactEntityAttributes';
 import { artifactQualityRevisionEntityAttributes } from 'common/artifactQualityRevisionEntityAttributes';
 
-import { IFields, useForm } from 'hooks/useForm';
+import { IFieldValues, useNewForm } from 'hooks/useNewForm';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 
 import { ActionModal } from 'components/ActionModal/ActionModal';
+import { FormInput } from 'components/FormInput/FormInput';
 
 import * as artifactApi from 'services/artifactApi';
 
@@ -35,7 +36,10 @@ export const ArtifactEditQualityModal = ({ isModalOpen, toggleModal, artifact, v
 
   const [isQualitySelectOpen, setIsQualitySelectOpen] = useState<boolean>(false);
 
-  const confirmModal = (data: IFields) => {
+  const { register, setFieldValues, getFieldState, getFieldErrors, handleSubmit, isSubmitDisabled, hasFormChanged } =
+    useNewForm();
+
+  const confirmModal = (data: IFieldValues) => {
     serviceContainerArtifactEditQuality
       .run({
         serviceData: {
@@ -43,8 +47,8 @@ export const ArtifactEditQualityModal = ({ isModalOpen, toggleModal, artifact, v
         },
         requestConfig: {
           params: {
-            quality: data.artifactQuality.value,
-            reason: data.qualityLevelReason.value,
+            quality: data.artifactQuality,
+            reason: data.qualityLevelReason,
           },
         },
       })
@@ -53,13 +57,11 @@ export const ArtifactEditQualityModal = ({ isModalOpen, toggleModal, artifact, v
       });
   };
 
-  const { fields, onChange, reinitialize, onSubmit, isSubmitDisabled, hasChanged } = useForm(formConfig, confirmModal);
-
   useEffect(() => {
-    reinitialize({
+    setFieldValues({
       artifactQuality: artifact.artifactQuality,
     });
-  }, [reinitialize, artifact.artifactQuality]);
+  }, [setFieldValues, artifact.artifactQuality]);
 
   return (
     <ActionModal
@@ -67,9 +69,9 @@ export const ArtifactEditQualityModal = ({ isModalOpen, toggleModal, artifact, v
       actionTitle="Edit Artifact Quality"
       isOpen={isModalOpen}
       isSubmitDisabled={isSubmitDisabled}
-      wereSubmitDataChanged={hasChanged}
+      wereSubmitDataChanged={hasFormChanged}
       onToggle={toggleModal}
-      onSubmit={onSubmit}
+      onSubmit={handleSubmit(confirmModal)}
       serviceContainer={serviceContainerArtifactEditQuality}
       modalVariant="large"
       onSuccessActions={
@@ -92,54 +94,62 @@ export const ArtifactEditQualityModal = ({ isModalOpen, toggleModal, artifact, v
           label={artifactQualityRevisionEntityAttributes.artifactQuality.title}
           fieldId={artifactQualityRevisionEntityAttributes.artifactQuality.id}
           helperText={
-            <FormHelperText isHidden={fields.artifactQuality.state !== 'error'} isError>
-              {fields.artifactQuality.errorMessages?.join(' ')}
+            <FormHelperText
+              isHidden={getFieldState(artifactQualityRevisionEntityAttributes.artifactQuality.id) !== 'error'}
+              isError
+            >
+              {getFieldErrors(artifactQualityRevisionEntityAttributes.artifactQuality.id)}
             </FormHelperText>
           }
         >
-          <Select
-            menuAppendTo="parent"
-            id={artifactQualityRevisionEntityAttributes.artifactQuality.id}
-            name={artifactQualityRevisionEntityAttributes.artifactQuality.id}
-            variant="single"
-            selections={fields.artifactQuality.value}
-            validated={fields.artifactQuality.state}
-            onSelect={(_, artifactQuality, isPlaceholder) => {
-              if (!isPlaceholder) {
-                onChange('artifactQuality', artifactQuality);
-                setIsQualitySelectOpen(false);
-              }
-            }}
-            onToggle={setIsQualitySelectOpen}
-            isOpen={isQualitySelectOpen}
-          >
-            {artifactEntityAttributes.artifactQuality.values.map((quality) => (
-              <SelectOption key={quality} value={quality} />
-            ))}
-          </Select>
+          <FormInput<string>
+            {...register<string>(artifactQualityRevisionEntityAttributes.artifactQuality.id, formConfig.artifactQuality)}
+            render={({ value, validated, onChange, onBlur }) => (
+              <Select
+                menuAppendTo="parent"
+                id={artifactQualityRevisionEntityAttributes.artifactQuality.id}
+                name={artifactQualityRevisionEntityAttributes.artifactQuality.id}
+                variant="single"
+                isOpen={isQualitySelectOpen}
+                selections={value}
+                validated={validated}
+                onToggle={setIsQualitySelectOpen}
+                onSelect={(_, artifactQuality, isPlaceholder) => {
+                  if (!isPlaceholder) {
+                    onChange(artifactQuality as string);
+                    setIsQualitySelectOpen(false);
+                  }
+                }}
+                onBlur={onBlur}
+              >
+                {artifactEntityAttributes.artifactQuality.values.map((quality) => (
+                  <SelectOption key={quality} value={quality} />
+                ))}
+              </Select>
+            )}
+          />
         </FormGroup>
         <FormGroup
           isRequired
           label={artifactQualityRevisionEntityAttributes.qualityLevelReason.title}
           fieldId={artifactQualityRevisionEntityAttributes.qualityLevelReason.id}
           helperText={
-            <FormHelperText isHidden={fields.qualityLevelReason.state !== 'error'} isError>
-              {fields.qualityLevelReason.errorMessages?.join(' ')}
+            <FormHelperText
+              isHidden={getFieldState(artifactQualityRevisionEntityAttributes.qualityLevelReason.id) !== 'error'}
+              isError
+            >
+              {getFieldErrors(artifactQualityRevisionEntityAttributes.qualityLevelReason.id)}
             </FormHelperText>
           }
         >
           <TextArea
             isRequired
+            type="text"
             id={artifactQualityRevisionEntityAttributes.qualityLevelReason.id}
             name={artifactQualityRevisionEntityAttributes.qualityLevelReason.id}
-            type="text"
-            value={fields.qualityLevelReason.value as string}
-            validated={fields.qualityLevelReason.state}
-            onChange={(qualityLevelReason) => {
-              onChange('qualityLevelReason', qualityLevelReason);
-            }}
             resizeOrientation="vertical"
             autoComplete="off"
+            {...register<string>(artifactQualityRevisionEntityAttributes.qualityLevelReason.id, formConfig.qualityLevelReason)}
           />
         </FormGroup>
       </Form>
