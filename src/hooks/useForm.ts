a33ebe9b0e27d8c72e323da3b354ac50900f72ea
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-02 15:54:26 +0200

    NCL-8162 Implement useForm async validations

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,8 +1,10 @@
 import { TextInputProps } from '@patternfly/react-core';
 import { AxiosError, isAxiosError } from 'axios';
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 import { PncError, isPncError } from 'common/PncError';
+
+import { isServiceError } from 'hooks/useServiceContainer';
 
 import { uiLogger } from 'services/uiLogger';
 
@@ -12,9 +14,12 @@ export type TValue = string | number | boolean;
 
 type TState = TextInputProps['validated'];
 
+type TValidationState = 'default' | 'validating' | 'error';
+
 interface IValidator {
-  validator: (value: any) => boolean;
-  errorMessage: string;
+  validator?: (value: any) => boolean;
+  asyncValidator?: (value: any) => Promise<any>;
+  errorMessage?: string;
   relatedFields?: string[];
 }
 
@@ -22,11 +27,12 @@ interface IField<T extends TValue> {
   value?: T;
   errorMessages?: string[];
   state?: TState;
+  validationState?: TValidationState;
   isRequired?: boolean;
   validators?: IValidator[];
 }
 
-export type TFieldConfig<T extends TValue> = Omit<IField<T>, 'errorMessages' | 'state'>;
+export type TFieldConfig<T extends TValue> = Omit<IField<T>, 'errorMessages' | 'state' | 'validationState'>;
 
 export interface IFields {
   [fieldName: string]: IField<TValue>;
@@ -45,6 +51,7 @@ export interface IRegisterData<T extends TValue> {
   onChange: OnChangeFunction<T>;
   onBlur: OnBlurFunction;
   validated: TState;
+  validationState?: TValidationState;
 }
 
 type OnSubmitCallback = (data: IFieldValues) => Promise<any>;
@@ -54,6 +61,8 @@ type OnSubmitFunction = () => void;
 export interface IFieldValues {
   [fieldName: string]: any;
 }
+
+const INPUT_VALIDATION_DELAY_MS = 200;
 
 /**
  * Hook to manage values, validation and states of form inputs.
@@ -68,9 +77,13 @@ export interface IFieldValues {
  * For multi-field validations (validating relationships between multiple fields), use validator with relatedFields property defined.
  * relatedFields specifies IDs of other fields that are in multi-field validation group.
  *
+ * For async validations (validating input on backend), use 'asyncValidator' property of the validator object.
+ * Async validations are executed after a delay (200 ms) to prevent unnecessary requests.
+ * Text inputs validated asynchronously should use dedicated component: {@link TextInputAsyncValidated}
+ *
  * See related components: {@link FormInput}
  *
- * See example usages: {@link ProjectCreateEditPage} {@link ScmRepositoryCreateEditPage}
+ * See example usages: {@link ProjectCreateEditPage} {@link ScmRepositoryCreateEditPage} {@link ProductMilestoneCreateEditPage}
  *
  * @returns object containing:
  *  - register         - function registering an input and returning its value, state and on-change function
@@ -88,24 +101,34 @@ export const useForm = () => {
   const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
   const [hasFormChanged, setHasFormChanged] = useState<boolean>(false);
 
+  const timeouts = useRef<{ [fieldName: string]: NodeJS.Timeout | undefined }>({});
+
   const register = <T extends TValue>(fieldName: string, fieldConfig?: TFieldConfig<T>): IRegisterData<T> => {
     if (!fields[fieldName]) {
-      setFields((fields) => ({ ...fields, [fieldName]: { ...fieldConfig, value: fieldConfig?.value || '', state: 'default' } }));
+      setFields((fields) => ({
+        ...fields,
+        [fieldName]: { ...fieldConfig, value: fieldConfig?.value || '', state: 'default', validationState: 'default' },
+      }));
     }
 
     return {
       value: fields[fieldName]?.value as T,
-      onChange: handleChange<T>(fieldName),
+      onChange: handleChange<T>(fieldName, fieldConfig),
       onBlur: handleBlur<T>(fieldName),
       validated: fields[fieldName]?.state,
+      validationState: fields[fieldName]?.validationState,
     };
   };
 
-  const handleChange = <T extends TValue>(fieldName: string): OnChangeFunction<T> => {
+  const handleChange = <T extends TValue>(fieldName: string, fieldConfig?: TFieldConfig<T>): OnChangeFunction<T> => {
     return (value: T) => {
+      const asyncValidators =
+        fieldConfig?.validators?.filter((validator) => validator.asyncValidator).map((validator) => validator.asyncValidator) ||
+        [];
+
       setFields((fields) => {
         const newFields = { ...fields };
-        newFields[fieldName] = constructNewFieldOnChange<T>(fieldName, newFields, value);
+        newFields[fieldName] = constructNewFieldOnChange<T>(fieldName, newFields, value, !!asyncValidators.length);
 
         // to prevent multiple updates when one field is contained in multiple relatedFields
         const updatedFields: string[] = [];
@@ -116,40 +139,93 @@ export const useForm = () => {
             multiFieldValidator.relatedFields!.forEach((relatedField) => {
               if (!updatedFields.includes(relatedField)) {
                 updatedFields.push(relatedField);
-                newFields[relatedField] = constructNewFieldOnChange<T>(relatedField, newFields);
+                newFields[relatedField] = constructNewFieldOnChange<T>(relatedField, newFields, undefined);
               }
             });
           });
 
-        const isFormValid = () => {
-          for (const fieldName in newFields) {
-            if (newFields[fieldName].errorMessages?.length) {
-              return false;
-            }
-          }
-
-          return true;
-        };
-
-        const areRequiredFilled = () => {
-          for (const fieldName in newFields) {
-            const oldValue = newFields[fieldName].value;
-            const value = typeof oldValue === 'string' ? oldValue?.trim() : oldValue;
-            if (newFields[fieldName].isRequired && !value) {
-              return false;
-            }
-          }
-
-          return true;
-        };
-
-        setIsSubmitDisabled(!isFormValid() || !areRequiredFilled());
+        if (!asyncValidators.length) {
+          setIsSubmitDisabled(!isFormValid(newFields) || !areRequiredFilled(newFields));
+        } else {
+          setIsSubmitDisabled(true);
+        }
 
         return newFields;
       });
 
+      if (asyncValidators.length) {
+        clearTimeout(timeouts.current[fieldName]);
+        timeouts.current[fieldName] = setTimeout(() => {
+          const oldTimeout = timeouts.current[fieldName];
+
+          Promise.all(
+            asyncValidators.map((asyncValidator) =>
+              asyncValidator!(value).then((response: any) => {
+                if (oldTimeout === timeouts.current[fieldName] && response?.data?.hints) {
+                  return response.data.hints;
+                }
+
+                return [];
+              })
+            )
+          )
+            .then((errorMessagesArrays: string[][]) => {
+              const errorMessages = errorMessagesArrays.flat();
+
+              setFields((fields) => {
+                const newFields = { ...fields };
+
+                newFields[fieldName] = constructNewFieldOnAsyncValidation(fieldName, newFields, errorMessages);
+
+                if (!errorMessages.length) {
+                  setIsSubmitDisabled(!isFormValid(newFields) || !areRequiredFilled(newFields));
+                }
+
+                return newFields;
+              });
+            })
+            .catch((error: any) => {
+              if (isServiceError(error)) {
+                setFields((fields) => {
+                  const newFields = { ...fields, [fieldName]: { ...fields[fieldName] } };
+
+                  newFields[fieldName].validationState = 'error';
+
+                  return newFields;
+                });
+              }
+            });
+        }, INPUT_VALIDATION_DELAY_MS);
+      }
+
       setHasFormChanged(true);
     };
+  };
+
+  const isFormValid = (fields: IFields) => {
+    for (const fieldName in fields) {
+      if (fields[fieldName].errorMessages?.length) {
+        return false;
+      }
+
+      if (fields[fieldName]?.validationState === 'validating') {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  const areRequiredFilled = (fields: IFields) => {
+    for (const fieldName in fields) {
+      const oldValue = fields[fieldName].value;
+      const value = typeof oldValue === 'string' ? oldValue?.trim() : oldValue;
+      if (fields[fieldName].isRequired && !value) {
+        return false;
+      }
+    }
+
+    return true;
   };
 
   const handleBlur = <T extends TValue>(fieldName: string): OnBlurFunction => {
@@ -164,14 +240,20 @@ export const useForm = () => {
     };
   };
 
-  const constructNewFieldOnChange = <T extends TValue>(fieldName: string, fields: IFields, newValue?: T): IField<T> => {
+  const constructNewFieldOnChange = <T extends TValue>(
+    fieldName: string,
+    fields: IFields,
+    newValue?: T,
+    isValidating?: boolean
+  ): IField<T> => {
     const newField = { ...(fields[fieldName] as IField<T>) };
 
     if (newValue !== undefined) {
       newField.value = constructNewFieldValue<T>(newValue);
     }
     newField.errorMessages = constructNewFieldErrorMessages(fieldName, { ...fields, [fieldName]: newField });
-    newField.state = constructNewFieldState(fieldName, { ...fields, [fieldName]: newField });
+    newField.state = constructNewFieldState(fieldName, { ...fields, [fieldName]: newField }, isValidating);
+    newField.validationState = isValidating ? 'validating' : 'default';
 
     return newField;
   };
@@ -192,6 +274,21 @@ export const useForm = () => {
     return newValue;
   };
 
+  const constructNewFieldOnAsyncValidation = <T extends TValue>(
+    fieldName: string,
+    fields: IFields,
+    newErrorMessages?: string[]
+  ): IField<T> => {
+    const newField = { ...(fields[fieldName] as IField<T>) };
+
+    const oldErrorMessages = newField.errorMessages ? newField.errorMessages : [];
+    newField.errorMessages = [...oldErrorMessages, ...(newErrorMessages ? newErrorMessages : [])];
+    newField.state = constructNewFieldState(fieldName, { ...fields, [fieldName]: newField });
+    newField.validationState = 'default';
+
+    return newField;
+  };
+
   const constructNewFieldErrorMessages = (fieldName: string, fields: IFields): string[] => {
     const value = fields[fieldName].value;
     const trimmedValue = typeof value === 'string' ? value.trim() : value;
@@ -207,7 +304,7 @@ export const useForm = () => {
       for (const validator of fields[fieldName].validators!) {
         const data = validator.relatedFields?.length ? transformFormToValues(fields) : trimmedValue;
 
-        if (!validator.validator(data)) {
+        if (validator.validator && validator.errorMessage && !validator.validator(data)) {
           newErrorMessages.push(validator.errorMessage);
         }
       }
@@ -216,16 +313,20 @@ export const useForm = () => {
     return newErrorMessages;
   };
 
-  const constructNewFieldState = (fieldName: string, fields: IFields): TState => {
+  const constructNewFieldState = (fieldName: string, fields: IFields, isValidating?: boolean): TState => {
     if (fields[fieldName]?.errorMessages?.length) {
       return 'error';
-    } else {
-      if (fields[fieldName].value) {
-        return 'success';
-      } else {
-        return 'default';
-      }
     }
+
+    if (isValidating) {
+      return 'default';
+    }
+
+    if (fields[fieldName].value) {
+      return 'success';
+    }
+
+    return 'default';
   };
 
   const setFieldValues = useCallback((fieldValues: IFieldValues) => {
