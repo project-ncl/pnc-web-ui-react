1e92972bf3205e5d33c166e01cab9c633d310784
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-04 10:23:42 +0200

    NCL-8167 Implement Build / Group Config entity recognition functions

--- src/utils/entityRecognition.ts
+++ src/utils/entityRecognition.ts
@@ -1,6 +1,14 @@
+import { BuildConfiguration, GroupConfiguration } from 'pnc-api-types-ts';
+
 export const isBuild = (possibleBuild: any) => 'buildConfigRevision' in possibleBuild;
 
 export const isGroupBuild = (possibleGroupBuild: any) => 'groupConfig' in possibleGroupBuild;
+
+export const isBuildConfig = (config: BuildConfiguration | GroupConfiguration): config is BuildConfiguration =>
+  'project' in config;
+
+export const isGroupConfig = (config: BuildConfiguration | GroupConfiguration): config is GroupConfiguration =>
+  'buildConfigs' in config;
 
 export const isProductMilestone = (possibleProductMilestone: any) => 'startingDate' in possibleProductMilestone;
 
