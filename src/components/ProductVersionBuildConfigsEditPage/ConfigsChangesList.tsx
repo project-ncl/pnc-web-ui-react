c620319f69d71c753f55097eaa0baebf9cf4b8fb
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-04 10:21:56 +0200

    NCL-8167 Reimplement  BuildConfigsChangesList  to ConfigsChangesList

--- src/components/ProductVersionBuildConfigsEditPage/ConfigsChangesList.tsx
+++ src/components/ProductVersionBuildConfigsEditPage/ConfigsChangesList.tsx
@@ -4,65 +4,83 @@ import { MinusCircleIcon, PlusCircleIcon } from '@patternfly/react-icons';
 import { CubesIcon } from '@patternfly/react-icons';
 import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 
-import { BuildConfiguration } from 'pnc-api-types-ts';
+import { BuildConfiguration, GroupConfiguration } from 'pnc-api-types-ts';
 
 import { buildConfigEntityAttributes } from 'common/buildConfigEntityAttributes';
 
 import { BuildConfigLink } from 'components/BuildConfigLink/BuildConfigLink';
 import { ContentBox } from 'components/ContentBox/ContentBox';
-import { IBuildConfigChange } from 'components/ProductVersionBuildConfigsEditPage/ProductVersionBuildConfigsEditPage';
 import { ProjectLink } from 'components/ProjectLink/ProjectLink';
 import { StateCard } from 'components/StateCard/StateCard';
 
-interface IBuildConfigsChangesListProps {
-  buildConfigChanges: IBuildConfigChange[];
-  onCancel: (buildConfig: BuildConfiguration) => void;
+import { isBuildConfig } from 'utils/entityRecognition';
+
+interface IConfigChange<T extends BuildConfiguration | GroupConfiguration> {
+  data: T;
+  operation: 'add' | 'remove';
+}
+
+export type TBuildConfigChange = IConfigChange<BuildConfiguration>;
+
+export type TGroupConfigChange = IConfigChange<GroupConfiguration>;
+
+interface IConfigsChangesListProps<T extends BuildConfiguration | GroupConfiguration> {
+  variant: 'Build' | 'Group Build';
+  configChanges: IConfigChange<T>[];
+  onCancel: (config: T) => void;
 }
 
 /**
- * Component displaying list of Build Configs to be added and/or removed.
+ * Component displaying list of Build/Group Configs to be added and/or removed.
  *
- * @param buildConfigChanges - List of Build Config add/remove operations
- * @param onCancel - Callback to cancel Build Config add/remove operation with
+ * @param variant - Build or Group Build variant
+ * @param configChanges - List of Build/Group Config add/remove operations
+ * @param onCancel - Callback to cancel Build/Group Config add/remove operation with
  */
-export const BuildConfigsChangesList = ({ buildConfigChanges, onCancel }: IBuildConfigsChangesListProps) => {
+export const ConfigsChangesList = <T extends BuildConfiguration | GroupConfiguration>({
+  variant,
+  configChanges,
+  onCancel,
+}: IConfigsChangesListProps<T>) => {
   return (
     <>
-      {buildConfigChanges.length ? (
+      {configChanges.length ? (
         <ContentBox borderTop>
           <TableComposable isStriped variant="compact">
             <Thead>
               <Tr>
                 <Th />
                 <Th width={60}>{buildConfigEntityAttributes.name.title}</Th>
-                <Th width={40}>{buildConfigEntityAttributes['project.name'].title}</Th>
+                {variant === 'Build' && <Th width={40}>{buildConfigEntityAttributes['project.name'].title}</Th>}
                 <Th />
               </Tr>
             </Thead>
             <Tbody>
-              {buildConfigChanges.map((buildConfigChange: any, rowIndex: number) => (
+              {configChanges.map((configChange: IConfigChange<T>, rowIndex: number) => (
                 <Tr key={rowIndex}>
                   <Td>
-                    {buildConfigChange.operation === 'add' ? (
+                    {configChange.operation === 'add' ? (
                       <PlusCircleIcon title="Marked to be added." color="green" />
                     ) : (
                       <MinusCircleIcon title="Marked to be removed." color="red" />
                     )}
                   </Td>
                   <Td>
-                    <BuildConfigLink id={buildConfigChange.data.id}>{buildConfigChange.data.name}</BuildConfigLink>
+                    <BuildConfigLink id={configChange.data.id}>{configChange.data.name}</BuildConfigLink>
                   </Td>
-                  <Td>
-                    {buildConfigChange.data.project && (
-                      <ProjectLink id={buildConfigChange.data.project.id}>{buildConfigChange.data.project.name}</ProjectLink>
-                    )}
-                  </Td>
+                  {isBuildConfig(configChange.data) && (
+                    <Td>
+                      {configChange.data.project && (
+                        <ProjectLink id={configChange.data.project.id}>{configChange.data.project.name}</ProjectLink>
+                      )}
+                    </Td>
+                  )}
                   <Td isActionCell>
                     <Button
                       variant="plain"
                       icon={<TimesCircleIcon />}
                       onClick={() => {
-                        onCancel(buildConfigChange.data);
+                        onCancel(configChange.data);
                       }}
                       isSmall
                       isInline
