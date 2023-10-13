3d75c73668363d826656d7621248ead5e0f90fb5
Martin Kelnar <mkelnar@redhat.com>
2023-10-13 15:03:12 +0200

    NCL-7210 Define build entity attributes

--- src/common/buildEntityAttributes.ts
+++ src/common/buildEntityAttributes.ts
@@ -1,5 +1,6 @@
 import { Build } from 'pnc-api-types-ts';
 
+import { buildConfigEntityAttributes } from 'common/buildConfigEntityAttributes';
 import { TEntityAttributes } from 'common/entityAttributes';
 
 const statusValues: Build['status'][] = [
@@ -20,16 +21,24 @@ interface IExtendedBuild extends Build {
   buildConfigName: any; // filtering only
   name: any; // derived from build and buildConfig
   'user.username': any;
+  buildType: string; // from buildConfigEntityAttributes
+  'buildConfigRevision.scmRevision': string;
+  scmBuildConfigRevision: string; // not released yet
+  'environment.description': string;
+  'buildConfigRevision.buildScript': string;
+  'buildConfigRevision.brewPullActive': string;
+  parameters: any;
 }
 
 export const buildEntityAttributes = {
   id: {
     id: 'id',
     title: 'ID',
+    tooltip: 'Unique identification of Build',
   },
   buildConfigName: {
     id: 'buildConfigName',
-    title: 'Build Config Name',
+    title: 'Build Config',
     filter: {
       operator: '=like=',
       isCustomParam: true,
@@ -63,6 +72,7 @@ export const buildEntityAttributes = {
   name: {
     id: 'name',
     title: 'Name',
+    tooltip: `Human readable title for Build derived from Build submitted and Build Config name. Uniqueness is not guaranteed, see ID instead.`,
   },
   submitTime: {
     id: 'submitTime',
@@ -84,5 +94,51 @@ export const buildEntityAttributes = {
     sort: {
       group: 'times',
     },
+  },
+  buildType: buildConfigEntityAttributes.buildType,
+  scmUrl: {
+    id: 'scmUrl',
+    title: 'SCM URL',
+  },
+  'buildConfigRevision.scmRevision': {
+    id: 'buildConfigRevision.scmRevision',
+    title: 'Pre-alignment SCM Ref',
+    tooltip:
+      'SCM Ref before Alignment is executed (the revision specified in the Build Config, previously named as Pre-alignment SCM Revision)',
+  },
+  scmBuildConfigRevision: {
+    id: 'scmBuildConfigRevision',
+    title: 'Pre-alignment SCM Revision',
+    tooltip: 'SCM Revision before Alignment is executed (the commit ID resolved from the Pre-alignment SCM Ref)',
+  },
+  scmTag: {
+    id: 'scmTag',
+    title: 'Post-alignment SCM Tag',
+    tooltip: 'SCM Tag after Alignment is executed (the tag created from PNC)',
+  },
+  scmRevision: {
+    id: 'scmRevision',
+    title: 'Post-alignment SCM Revision',
+    tooltip: 'SCM Revision after Alignment is executed (the commit ID resolved from the Post-alignment SCM Tag)',
+  },
+  'environment.description': {
+    id: 'environment.description',
+    title: 'Environment',
+  },
+  'buildConfigRevision.buildScript': {
+    id: 'buildConfigRevision.buildScript',
+    title: 'Build Script',
+  },
+  attributes: {
+    id: 'attributes',
+    title: 'Parameters',
+  },
+  'buildConfigRevision.brewPullActive': {
+    id: 'buildConfigRevision.brewPullActive',
+    title: 'Brew Pull Active',
+  },
+  parameters: {
+    id: 'parameters',
+    title: 'Parameters',
   },
 } as const satisfies TEntityAttributes<IExtendedBuild>;
