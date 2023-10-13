ab9271e8e09f173df81c6ffd7e91c8421eac9fb0
Martin Kelnar <mkelnar@redhat.com>
2023-10-13 15:06:20 +0200

    NCL-7210 Implement getRevision buildConfigAPI

--- src/services/buildConfigApi.ts
+++ src/services/buildConfigApi.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 
-import { Build, BuildConfigPage } from 'pnc-api-types-ts';
+import { Build, BuildConfigPage, BuildConfigurationRevision } from 'pnc-api-types-ts';
 
 import { pncClient } from './pncClient';
 
@@ -40,4 +40,21 @@ export const getBuildConfigsWithLatestBuild = (requestConfig: AxiosRequestConfig
 export const build = ({ buildStartParams }: { buildStartParams: IBuildStartParams }, requestConfig: AxiosRequestConfig = {}) => {
   requestConfig.params = requestConfig.params ? Object.assign(requestConfig.params, buildStartParams) : buildStartParams;
   return pncClient.getHttpClient().post<Build>(`/build-configs/${buildStartParams.id}/build`, null, requestConfig);
+};
+
+/**
+ * Gets specific audited revision of Build Config.
+ *
+ * @param serviceData - object containing:
+ *  - buildConfigId - ID of the Build Config
+ *  - buildConfigRev - Revision number of the Build Config
+ * @param requestConfig - Axios based request config
+ */
+export const getRevision = (
+  { buildConfigId, buildConfigRev }: { buildConfigId: string; buildConfigRev: number },
+  requestConfig: AxiosRequestConfig = {}
+) => {
+  return pncClient
+    .getHttpClient()
+    .get<BuildConfigurationRevision>(`/build-configs/${buildConfigId}/revisions/${buildConfigRev}`, requestConfig);
 };
