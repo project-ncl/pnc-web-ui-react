d90407ff90fbd6e4935c844cd294af7185c9aa57
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-17 15:23:47 +0200

    NCL-8235 Integrate full TypeScript type support into service container

--- src/components/ServiceContainers/ServiceContainerCreatingUpdating.tsx
+++ src/components/ServiceContainers/ServiceContainerCreatingUpdating.tsx
@@ -1,26 +1,26 @@
 import { Alert } from '@patternfly/react-core';
-import React from 'react';
+import { PropsWithChildren } from 'react';
 
-import { DataValues, IServiceContainer } from 'hooks/useServiceContainer';
+import { DataValues, IServiceContainerState, TServiceData } from 'hooks/useServiceContainer';
 
 import { IServiceContainerProps, ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
 import { RefreshStateCard } from 'components/StateCard/RefreshStateCard';
 
-interface IServiceContainerCreatingUpdatingProps extends IServiceContainerProps {
+interface IServiceContainerCreatingUpdatingProps<T extends TServiceData> extends IServiceContainerProps<T> {
   title?: string;
   /**
    * Typically only edit pages pass serviceContainerLoading to handle initial data states (like loading or error)
    */
-  serviceContainerLoading?: IServiceContainer;
+  serviceContainerLoading?: IServiceContainerState<T>;
 }
 
-export const ServiceContainerCreatingUpdating = ({
+export const ServiceContainerCreatingUpdating = <T extends TServiceData>({
   loading,
   error,
   title,
   serviceContainerLoading,
   children,
-}: React.PropsWithChildren<IServiceContainerCreatingUpdatingProps>) => {
+}: PropsWithChildren<IServiceContainerCreatingUpdatingProps<T>>) => {
   // Edit pages only
   if (serviceContainerLoading && (serviceContainerLoading.loading || serviceContainerLoading.error)) {
     // no child content is needed, only initial data loading states are handled, once data is loaded, edit page content is displayed instead
--- src/hooks/useServiceContainer.ts
+++ src/hooks/useServiceContainer.ts
@@ -1,20 +1,35 @@
-import { AxiosError, AxiosRequestConfig, isAxiosError } from 'axios';
+import { AxiosError, AxiosRequestConfig, AxiosResponse, isAxiosError } from 'axios';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-export interface IServiceContainer {
-  data: any;
-  loading: boolean;
-  error: string;
-  loadingStateDelayMs: number; // in milliseconds
-  run: ServiceContainerRunnerFunction;
+export type TServiceData = Object | undefined;
+
+interface IServiceDataPaginated<T extends Object> {
+  content: T[];
+  pageIndex?: number;
+  pageSize?: number;
+  totalHits?: number;
+  totalPages?: number;
 }
 
-export interface IService<T = {}> {
+export type TServiceParams = Object | undefined;
+
+type ServiceFunctionWithParams<T extends TServiceData, U extends TServiceParams> = (
+  serviceData: U,
+  requestConfig?: AxiosRequestConfig
+) => Promise<AxiosResponse<T>>;
+
+type ServiceFunctionWithoutParams<T extends TServiceData> = (requestConfig?: AxiosRequestConfig) => Promise<AxiosResponse<T>>;
+
+type ServiceFunction<T extends TServiceData, U extends TServiceParams> =
+  | ServiceFunctionWithParams<T, U>
+  | ServiceFunctionWithoutParams<T>;
+
+export interface IService<U extends TServiceParams> {
   /**
    * Service data, eg { id: '2' }
    */
-  serviceData?: T;
+  serviceData?: U;
 
   /**
    * Axios based request config, eg { signal: <abortingSignal> }
@@ -22,7 +37,27 @@ export interface IService<T = {}> {
   requestConfig?: AxiosRequestConfig;
 }
 
-export type ServiceContainerRunnerFunction = (iService?: IService<Object | null>) => any;
+export type ServiceContainerRunnerFunction<T extends TServiceData, U extends TServiceParams> = (
+  iService?: IService<U>
+) => Promise<AxiosResponse<T>>;
+
+/**
+ * Use when only data and state of the service is needed and possibly, 'run' function params are unknown.
+ */
+export interface IServiceContainerState<T extends TServiceData> {
+  data: T | undefined | null;
+  loading: boolean;
+  error: string;
+  loadingStateDelayMs: number; // in milliseconds
+}
+
+/**
+ * Unlike IServiceContainerState, also service 'run' function is provided.
+ * When service need to be executed, prefer to use this interface over IServiceContainerState.
+ */
+export interface IServiceContainer<T extends TServiceData, U extends TServiceParams> extends IServiceContainerState<T> {
+  run: ServiceContainerRunnerFunction<T, U>;
+}
 
 export const DataValues = {
   /**
@@ -47,11 +82,14 @@ export const DataValues = {
  * @param loadingStateDelayMs - Waiting time before loading state is activated (in milliseconds)
  * @returns Object with data, loading and error property
  */
-export const useServiceContainer = (service: Function, loadingStateDelayMs: number = 200): IServiceContainer => {
+export const useServiceContainer = <T extends TServiceData, U extends TServiceParams>(
+  service: ServiceFunction<T, U>,
+  loadingStateDelayMs: number = 200
+): IServiceContainer<T, U> => {
   const ERROR_INIT: string = '';
 
   // initial states when component is loaded for the first time
-  const [data, setData] = useState<any>(DataValues.notYetData);
+  const [data, setData] = useState<IServiceContainerState<T>['data']>(DataValues.notYetData);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>(ERROR_INIT);
 
@@ -64,7 +102,7 @@ export const useServiceContainer = (service: Function, loadingStateDelayMs: numb
     };
   }, []);
 
-  const serviceContainerRunner = ({ serviceData = null, requestConfig = {} }: IService<Object | null> = {}) => {
+  const serviceContainerRunner = ({ serviceData = undefined, requestConfig = {} }: IService<U> = {}) => {
     loadingCount.current++;
 
     // set delayed (delayed to prevent flashing experience and unnecessary renders) loading state
@@ -85,8 +123,12 @@ export const useServiceContainer = (service: Function, loadingStateDelayMs: numb
     lastAbortController.current = new AbortController();
     requestConfig.signal = lastAbortController.current.signal;
 
-    return (serviceData ? service(serviceData, requestConfig) : service(requestConfig))
-      .then((response: any) => {
+    return (
+      serviceData
+        ? (service as ServiceFunctionWithParams<T, U>)(serviceData, requestConfig)
+        : (service as ServiceFunctionWithoutParams<T>)(requestConfig)
+    )
+      .then((response) => {
         // In a future React version (potentially in React 17) this could be removed as it will be default behavior
         // https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates/48610973#48610973
         ReactDOM.unstable_batchedUpdates(() => {
@@ -103,6 +145,7 @@ export const useServiceContainer = (service: Function, loadingStateDelayMs: numb
           }
           setError(ERROR_INIT);
         });
+
         return response;
       })
       .catch((error: Error | AxiosError) => {
@@ -119,8 +162,9 @@ export const useServiceContainer = (service: Function, loadingStateDelayMs: numb
               setData(DataValues.noData);
             });
           }
-          throw error;
         }
+
+        throw error;
       })
       .finally(() => {
         loadingCount.current--;
@@ -163,3 +207,6 @@ const getErrorMessage = (error: Error | AxiosError): string => {
 
   return error.message;
 };
+
+export const areServiceDataPaginated = (data: IServiceContainerState<Object>['data']): data is IServiceDataPaginated<Object> =>
+  !!data && typeof data === 'object' && 'content' in data;
