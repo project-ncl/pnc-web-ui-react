48f12dd6a782ba7ccebfbe5fa28e8ca9ca01c659
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-23 18:43:17 +0200

    NCL-8118 Implement Build Log page

--- src/components/BuildLogPage/BuildLogPage.tsx
+++ src/components/BuildLogPage/BuildLogPage.tsx
@@ -1,5 +1,95 @@
+import {
+  Button,
+  DescriptionList,
+  DescriptionListDescription,
+  DescriptionListGroup,
+  DescriptionListTerm,
+  Popover,
+} from '@patternfly/react-core';
+import { useEffect, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useServiceContainer } from 'hooks/useServiceContainer';
+
+import { BuildLogLink } from 'components/BuildLogLink/BuildLogLink';
+import { useServiceContainerBuild } from 'components/BuildPages/BuildPages';
 import { ContentBox } from 'components/ContentBox/ContentBox';
+import { LogViewer } from 'components/LogViewer/LogViewer';
+import { ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
+import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
+
+import * as buildApi from 'services/buildApi';
+import { userService } from 'services/userService';
 
 export const BuildLogPage = () => {
-  return <ContentBox padding></ContentBox>;
+  const { buildId } = useParams();
+
+  const { serviceContainerBuild } = useServiceContainerBuild();
+
+  const serviceContainerBuildLog = useServiceContainer(buildApi.getBuildLog);
+  const serviceContainerBuildLogRunner = serviceContainerBuildLog.run;
+
+  const serviceContainerBuildSshCredentials = useServiceContainer(buildApi.getSshCredentials);
+  const serviceContainerBuildSshCredentialsRunner = serviceContainerBuildSshCredentials.run;
+
+  const belongsToCurrentUser = useMemo(
+    () => userService.getUserId() === serviceContainerBuild.data?.user?.id,
+    [serviceContainerBuild.data]
+  );
+  const logData = useMemo(() => serviceContainerBuildLog.data?.split(/[\r\n]/), [serviceContainerBuildLog.data]);
+
+  useEffect(() => {
+    serviceContainerBuildLogRunner({ serviceData: { id: buildId } });
+    if (belongsToCurrentUser) {
+      serviceContainerBuildSshCredentialsRunner({ serviceData: { id: buildId } });
+    }
+  }, [serviceContainerBuildLogRunner, serviceContainerBuildSshCredentialsRunner, buildId, belongsToCurrentUser]);
+
+  const logActions = [
+    belongsToCurrentUser
+      ? [
+          <ServiceContainerLoading {...serviceContainerBuildSshCredentials} variant="icon" title="SSH credentials">
+            <Popover
+              removeFindDomNode
+              position="right-end"
+              bodyContent={
+                <DescriptionList isHorizontal isCompact>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>command:</DescriptionListTerm>
+                    <DescriptionListDescription>{serviceContainerBuildSshCredentials.data?.command}</DescriptionListDescription>
+                  </DescriptionListGroup>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>password:</DescriptionListTerm>
+                    <DescriptionListDescription>{serviceContainerBuildSshCredentials.data?.password}</DescriptionListDescription>
+                  </DescriptionListGroup>
+                </DescriptionList>
+              }
+            >
+              <TooltipWrapper
+                tooltip={
+                  !serviceContainerBuildSshCredentials.data?.command
+                    ? "SSH credentials are only available for those unsuccessful builds with 'keep pod alive' option. Alternatively you can modify your build script to intentionally fail it and get the SSH credentials you need."
+                    : undefined
+                }
+              >
+                <Button variant="control" isAriaDisabled={!serviceContainerBuildSshCredentials.data?.command}>
+                  SSH Credentials
+                </Button>
+              </TooltipWrapper>
+            </Popover>
+          </ServiceContainerLoading>,
+        ]
+      : [],
+    <BuildLogLink buildId={buildId!} />,
+  ];
+
+  return (
+    <ContentBox>
+      <ServiceContainerLoading {...serviceContainerBuildLog} allowEmptyData title="Build Log">
+        <ContentBox padding>
+          <LogViewer isStatic data={logData} customActions={logActions} />
+        </ContentBox>
+      </ServiceContainerLoading>
+    </ContentBox>
+  );
 };
