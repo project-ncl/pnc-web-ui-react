5074b4fc09b173f9c1b20a20cdd21d9d011c7e2e
Patrik Korytar <patrik.korytar@gmail.com>
2023-10-24 17:05:57 +0200

    NCL-8116 Implement Build metrics page

--- src/components/BuildMetricsPage/BuildMetricsPage.tsx
+++ src/components/BuildMetricsPage/BuildMetricsPage.tsx
@@ -1,5 +1,17 @@
+import { BuildMetrics } from 'components/BuildMetrics/BuildMetrics';
+import { useServiceContainerBuild } from 'components/BuildPages/BuildPages';
 import { ContentBox } from 'components/ContentBox/ContentBox';
 
-export const BuildMetricsPage = () => {
-  return <ContentBox padding></ContentBox>;
+interface IBuildMetricsPageProps {
+  componentId?: string;
+}
+
+export const BuildMetricsPage = ({ componentId = 'm1' }: IBuildMetricsPageProps) => {
+  const { serviceContainerBuild } = useServiceContainerBuild();
+
+  return (
+    <ContentBox padding>
+      <BuildMetrics builds={[serviceContainerBuild.data]} chartType="horizontalBar" componentId={componentId} />
+    </ContentBox>
+  );
 };
