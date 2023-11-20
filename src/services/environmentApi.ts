512234ed4e409b56c1c457d359079d661d5b6913
Patrik Korytar <patrik.korytar@gmail.com>
2023-11-20 12:21:47 +0100

    NCL-7207 Implement environment REST API services

---
+++ src/services/environmentApi.ts
@@ -0,0 +1,61 @@
+import { AxiosRequestConfig } from 'axios';
+
+import { BuildEnvironmentPage, Environment } from 'pnc-api-types-ts';
+
+import { extendRequestConfig } from 'utils/requestConfigHelper';
+
+import { pncClient } from './pncClient';
+
+export interface IEnvironmentApiData {
+  id: string;
+}
+
+/**
+ * Gets all Environments.
+ *
+ * @param requestConfig - Axios based request config
+ */
+export const getEnvironments = (
+  { hidden, deprecated }: { hidden?: boolean; deprecated?: boolean },
+  requestConfig: AxiosRequestConfig = {}
+) => {
+  return pncClient.getHttpClient().get<BuildEnvironmentPage>(
+    '/environments',
+    extendRequestConfig({
+      originalConfig: requestConfig,
+      newParams: {
+        qItems: [
+          ...(hidden !== undefined
+            ? [
+                {
+                  id: 'hidden',
+                  value: hidden,
+                  operator: '==',
+                },
+              ]
+            : []),
+          ...(deprecated !== undefined
+            ? [
+                {
+                  id: 'deprecated',
+                  value: deprecated,
+                  operator: '==',
+                },
+              ]
+            : []),
+        ],
+      },
+    })
+  );
+};
+
+/**
+ * Gets a specific Environment.
+ *
+ * @param serviceData - object containing:
+ *  - id - Environment ID
+ * @param requestConfig - Axios based request config
+ */
+export const getBuild = ({ id }: IEnvironmentApiData, requestConfig: AxiosRequestConfig = {}) => {
+  return pncClient.getHttpClient().get<Environment>(`/environments/${id}`, requestConfig);
+};
