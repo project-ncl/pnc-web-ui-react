79e8b47c7f449cd36b8daffac831c5e13a0f0268
Patrik Korytar <patrik.korytar@gmail.com>
2023-11-20 12:18:01 +0100

    NCL-7207 Implement ExpandableSection component

---
+++ src/components/ExpandableSection/ExpandableSection.tsx
@@ -0,0 +1,23 @@
+import { ExpandableSection as ExpandableSectionPF } from '@patternfly/react-core';
+import { PropsWithChildren } from 'react';
+
+export interface IExpandableSectionProps {
+  title: string;
+  isExpanded: boolean;
+  onToggle: (isExpanded: boolean) => void;
+}
+
+/**
+ * Section of content that can be collapsed and expanded.
+ * Can be used as a form section.
+ *
+ * @param title - Title of the section
+ * @param isExpanded - Section expand state
+ * @param onToggle - Callback to change the expand state with
+ * @param children - Section contents
+ */
+export const ExpandableSection = ({ children, title, isExpanded, onToggle }: PropsWithChildren<IExpandableSectionProps>) => (
+  <ExpandableSectionPF toggleText={title} isExpanded={isExpanded} onToggle={onToggle}>
+    {children}
+  </ExpandableSectionPF>
+);
