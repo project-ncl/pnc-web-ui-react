ef043afc2c597ac5a06f1478f1a7d2cf2afd8b8c
Dennis Zhou <tozhou@redhat.com>
2023-12-04 11:12:50 +0000

    NCL-8119 Implement BuildConfigDependencies tab

---
+++ src/components/BuildConfigDependenciesPage/BuildConfigDependenciesPage.tsx
@@ -0,0 +1,43 @@
+import { Text, TextContent, TextVariants, ToolbarItem } from '@patternfly/react-core';
+
+import { useParamsRequired } from 'hooks/useParamsRequired';
+import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
+import { useServiceContainer } from 'hooks/useServiceContainer';
+
+import { ActionButton } from 'components/ActionButton/ActionButton';
+import { BuildConfigsList } from 'components/BuildConfigsList/BuildConfigsList';
+import { ProtectedComponent } from 'components/ProtectedContent/ProtectedComponent';
+import { Toolbar } from 'components/Toolbar/Toolbar';
+
+import * as buildConfigApi from 'services/buildConfigApi';
+
+export const BuildConfigDependenciesPage = ({ componentId = 'bcd1' }) => {
+  const { buildConfigId } = useParamsRequired();
+
+  const serviceContainerDependencies = useServiceContainer(buildConfigApi.getDependencies);
+  const serviceContainerDependenciesRunner = serviceContainerDependencies.run;
+
+  useQueryParamsEffect(
+    ({ requestConfig } = {}) => serviceContainerDependenciesRunner({ serviceData: { id: buildConfigId }, requestConfig }),
+    { componentId }
+  );
+
+  return (
+    <>
+      <Toolbar borderBottom>
+        <ToolbarItem>
+          <TextContent>
+            <Text component={TextVariants.h2}>Dependencies</Text>
+          </TextContent>
+        </ToolbarItem>
+        <ToolbarItem>
+          <ProtectedComponent>
+            <ActionButton link="edit">Edit</ActionButton>
+          </ProtectedComponent>
+        </ToolbarItem>
+      </Toolbar>
+
+      <BuildConfigsList componentId={componentId} serviceContainerBuildConfigs={serviceContainerDependencies} />
+    </>
+  );
+};
