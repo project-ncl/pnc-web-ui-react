bb51b023bd111a4144f51e2b7d98e6308f472755
Patrik Korytar <patrik.korytar@gmail.com>
2023-12-04 15:12:22 +0100

    NCL-7936 Add isVisible prop to TooltipWrapper component

--- src/components/TooltipWrapper/TooltipWrapper.tsx
+++ src/components/TooltipWrapper/TooltipWrapper.tsx
@@ -4,16 +4,18 @@ import { ReactNode } from 'react';
 
 interface ITooltipWrapperProps {
   tooltip?: ReactNode;
+  isVisible?: boolean;
 }
 /**
  * Tooltip Wrapper to make any component (like icon) to become a tooltip component.
  *
  * @param tooltip - the tooltip to be displayed
+ * @param isVisible - set the tooltip visibility programmatically
  * @param children - the tooltip handler, by default <InfoCircleIcon />
  */
-export const TooltipWrapper = ({ tooltip, children }: React.PropsWithChildren<ITooltipWrapperProps>) =>
+export const TooltipWrapper = ({ tooltip, isVisible, children }: React.PropsWithChildren<ITooltipWrapperProps>) =>
   tooltip ? (
-    <Tooltip removeFindDomNode content={tooltip}>
+    <Tooltip removeFindDomNode content={tooltip} isVisible={isVisible}>
       <> {children ? <>{children}</> : <InfoCircleIcon />}</>
     </Tooltip>
   ) : (
