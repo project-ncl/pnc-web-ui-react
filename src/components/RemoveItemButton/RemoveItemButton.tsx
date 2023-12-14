59c9a6a9d05721ef80d43e6c0e66da9446198878
Patrik Korytar <patrik.korytar@gmail.com>
2023-12-14 15:02:21 +0100

    NCL-7207 Implement remove item button

---
+++ src/components/RemoveItemButton/RemoveItemButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@patternfly/react-core';
+import { TimesCircleIcon } from '@patternfly/react-icons';
+
+interface IRemoveItemButtonProps {
+  onRemove: () => void;
+}
+
+export const RemoveItemButton = ({ onRemove }: IRemoveItemButtonProps) => (
+  <Button variant="plain" icon={<TimesCircleIcon />} onClick={onRemove} isSmall isInline />
+);
