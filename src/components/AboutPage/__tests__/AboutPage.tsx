f1b6a50f6d84405cd44b9101e7f6b1d89499ed83
Patrik Korytar <patrik.korytar@gmail.com>
2024-01-08 17:14:28 +0100

    NCL-8339 Remove About Page test

--- src/components/AboutPage/__tests__/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import { AboutPage } from 'components/AboutPage/AboutPage';
-
-jest.mock('services/genericSettingsApi');
-
-test('renders AdministrationPage', () => {
-  render(<AboutPage />);
-});
