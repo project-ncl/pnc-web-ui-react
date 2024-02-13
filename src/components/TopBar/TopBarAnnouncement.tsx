1623e656812761b08708bcab3628f7f845e9e051
Patrik Korytar <patrik.korytar@gmail.com>
2024-02-13 11:35:39 +0100

    NCL-7620 Rework Top Bar statuses to be stored in session storage

--- src/components/TopBar/TopBarAnnouncement.tsx
+++ src/components/TopBar/TopBarAnnouncement.tsx
@@ -3,19 +3,21 @@ import { ExclamationTriangleIcon, InfoCircleIcon } from '@patternfly/react-icons
 import { TOPBAR_TYPE, TopBar } from 'components/TopBar/TopBar';
 
 interface ITopBarAnnouncementProps {
+  id?: string;
   banner?: string;
   isMaintenanceMode?: boolean;
   eta?: string;
   hideCloseButton?: boolean;
 }
 
-export const TopBarAnnouncement = ({ banner, isMaintenanceMode, eta, hideCloseButton }: ITopBarAnnouncementProps) => {
+export const TopBarAnnouncement = ({ id, banner, isMaintenanceMode, eta, hideCloseButton }: ITopBarAnnouncementProps) => {
   if (!banner && !isMaintenanceMode) {
     return null;
   }
 
   return (
     <TopBar
+      id={id}
       type={isMaintenanceMode ? TOPBAR_TYPE.Warning : TOPBAR_TYPE.Info}
       icon={isMaintenanceMode ? <ExclamationTriangleIcon /> : <InfoCircleIcon />}
       hideCloseButton={hideCloseButton}
