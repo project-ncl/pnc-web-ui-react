7ac4a3342e98efe8b44b7b59ee1c20750b019a75
Patrik Korytar <patrik.korytar@gmail.com>
2024-02-19 13:02:47 +0100

    NCL-8366 Integrate new Version endpoints on About page

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -21,6 +21,9 @@ export const AboutPage = () => {
   const serviceContainerKafkaVersion = useServiceContainer(versionApi.getKafkaVersion);
   const serviceContainerKafkaVersionRunner = serviceContainerKafkaVersion.run;
 
+  const serviceContainerCausewayVersion = useServiceContainer(versionApi.getCausewayVersion);
+  const serviceContainerCausewayVersionRunner = serviceContainerCausewayVersion.run;
+
   const serviceContainerUiLoggerVersion = useServiceContainer(versionApi.getUiLoggerVersion);
   const serviceContainerUiLoggerVersionRunner = serviceContainerUiLoggerVersion.run;
 
@@ -33,20 +36,62 @@ export const AboutPage = () => {
   const serviceContainerDependencyAnalyzerVersion = useServiceContainer(versionApi.getDependencyAnalyzerVersion);
   const serviceContainerDependencyAnalyzerVersionRunner = serviceContainerDependencyAnalyzerVersion.run;
 
+  const serviceContainerBuildDriverVersion = useServiceContainer(versionApi.getBuildDriverVersion);
+  const serviceContainerBuildDriverVersionRunner = serviceContainerBuildDriverVersion.run;
+
+  const serviceContainerCleanerVersion = useServiceContainer(versionApi.getCleanerVersion);
+  const serviceContainerCleanerVersionRunner = serviceContainerCleanerVersion.run;
+
+  const serviceContainerDeliverablesAnalyzerVersion = useServiceContainer(versionApi.getDeliverablesAnalyzerVersion);
+  const serviceContainerDeliverablesAnalyzerVersionRunner = serviceContainerDeliverablesAnalyzerVersion.run;
+
+  const serviceContainerEnvironmentDriverVersion = useServiceContainer(versionApi.getEnvironmentDriverVersion);
+  const serviceContainerEnvironmentDriverVersionRunner = serviceContainerEnvironmentDriverVersion.run;
+
+  const serviceContainerLogEventDurationVersion = useServiceContainer(versionApi.getLogEventDurationVersion);
+  const serviceContainerLogEventDurationVersionRunner = serviceContainerLogEventDurationVersion.run;
+
+  const serviceContainerRepositoryDriverVersion = useServiceContainer(versionApi.getRepositoryDriverVersion);
+  const serviceContainerRepositoryDriverVersionRunner = serviceContainerRepositoryDriverVersion.run;
+
+  const serviceContainerRexVersion = useServiceContainer(versionApi.getRexVersion);
+  const serviceContainerRexVersionRunner = serviceContainerRexVersion.run;
+
+  const serviceContainerEttVersion = useServiceContainer(versionApi.getEttVersion);
+  const serviceContainerEttVersionRunner = serviceContainerEttVersion.run;
+
   useEffect(() => {
     serviceContainerPncVersionRunner();
     serviceContainerKafkaVersionRunner();
+    serviceContainerCausewayVersionRunner();
     serviceContainerUiLoggerVersionRunner();
     serviceContainerRepourVersionRunner();
     serviceContainerBifrostVersionRunner();
     serviceContainerDependencyAnalyzerVersionRunner();
+    serviceContainerBuildDriverVersionRunner();
+    serviceContainerCleanerVersionRunner();
+    serviceContainerDeliverablesAnalyzerVersionRunner();
+    serviceContainerEnvironmentDriverVersionRunner();
+    serviceContainerLogEventDurationVersionRunner();
+    serviceContainerRepositoryDriverVersionRunner();
+    serviceContainerRexVersionRunner();
+    serviceContainerEttVersionRunner();
   }, [
     serviceContainerPncVersionRunner,
     serviceContainerKafkaVersionRunner,
+    serviceContainerCausewayVersionRunner,
     serviceContainerUiLoggerVersionRunner,
     serviceContainerRepourVersionRunner,
     serviceContainerBifrostVersionRunner,
     serviceContainerDependencyAnalyzerVersionRunner,
+    serviceContainerBuildDriverVersionRunner,
+    serviceContainerCleanerVersionRunner,
+    serviceContainerDeliverablesAnalyzerVersionRunner,
+    serviceContainerEnvironmentDriverVersionRunner,
+    serviceContainerLogEventDurationVersionRunner,
+    serviceContainerRepositoryDriverVersionRunner,
+    serviceContainerRexVersionRunner,
+    serviceContainerEttVersionRunner,
   ]);
 
   return (
@@ -97,6 +142,23 @@ export const AboutPage = () => {
                 revision={serviceContainerKafkaVersion.data?.commit}
                 repositoryUrl={RepositoryUrls.kafkaRepository}
                 builtOn={serviceContainerKafkaVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.causewayRepository} target="_blank" rel="noopener noreferrer">
+                Causeway
+              </a>
+            }
+          >
+            <ServiceContainerLoading {...serviceContainerCausewayVersion} variant="inline" title="Causeway version">
+              <VersionText
+                version={serviceContainerCausewayVersion.data?.version}
+                revision={serviceContainerCausewayVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.causewayRepository}
+                builtOn={serviceContainerCausewayVersion.data?.builtOn}
               />
             </ServiceContainerLoading>
           </AttributesItem>
@@ -169,6 +231,151 @@ export const AboutPage = () => {
                 revision={serviceContainerDependencyAnalyzerVersion.data?.commit}
                 repositoryUrl={RepositoryUrls.dependencyAnalyzerRepository}
                 builtOn={serviceContainerDependencyAnalyzerVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.buildDriverRepository} target="_blank" rel="noopener noreferrer">
+                Build Driver
+              </a>
+            }
+          >
+            <ServiceContainerLoading {...serviceContainerBuildDriverVersion} variant="inline" title="Build Driver version">
+              <VersionText
+                version={serviceContainerBuildDriverVersion.data?.version}
+                revision={serviceContainerBuildDriverVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.buildDriverRepository}
+                builtOn={serviceContainerBuildDriverVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.cleanerRepository} target="_blank" rel="noopener noreferrer">
+                Cleaner
+              </a>
+            }
+          >
+            <ServiceContainerLoading {...serviceContainerCleanerVersion} variant="inline" title="Cleaner version">
+              <VersionText
+                version={serviceContainerCleanerVersion.data?.version}
+                revision={serviceContainerCleanerVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.cleanerRepository}
+                builtOn={serviceContainerCleanerVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.deliverablesAnalyzerRepository} target="_blank" rel="noopener noreferrer">
+                Deliverables Analyzer
+              </a>
+            }
+          >
+            <ServiceContainerLoading
+              {...serviceContainerDeliverablesAnalyzerVersion}
+              variant="inline"
+              title="Deliverables Analyzer version"
+            >
+              <VersionText
+                version={serviceContainerDeliverablesAnalyzerVersion.data?.version}
+                revision={serviceContainerDeliverablesAnalyzerVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.deliverablesAnalyzerRepository}
+                builtOn={serviceContainerDeliverablesAnalyzerVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.environmentDriverRepository} target="_blank" rel="noopener noreferrer">
+                Environment Driver
+              </a>
+            }
+          >
+            <ServiceContainerLoading
+              {...serviceContainerEnvironmentDriverVersion}
+              variant="inline"
+              title="Environment Driver version"
+            >
+              <VersionText
+                version={serviceContainerEnvironmentDriverVersion.data?.version}
+                revision={serviceContainerEnvironmentDriverVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.environmentDriverRepository}
+                builtOn={serviceContainerEnvironmentDriverVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.logEventDurationRepository} target="_blank" rel="noopener noreferrer">
+                Log Event Duration
+              </a>
+            }
+          >
+            <ServiceContainerLoading
+              {...serviceContainerLogEventDurationVersion}
+              variant="inline"
+              title="Log Event Duration version"
+            >
+              <VersionText
+                version={serviceContainerLogEventDurationVersion.data?.version}
+                revision={serviceContainerLogEventDurationVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.logEventDurationRepository}
+                builtOn={serviceContainerLogEventDurationVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.repositoryDriverRepository} target="_blank" rel="noopener noreferrer">
+                Repository Driver
+              </a>
+            }
+          >
+            <ServiceContainerLoading
+              {...serviceContainerRepositoryDriverVersion}
+              variant="inline"
+              title="Repository Driver version"
+            >
+              <VersionText
+                version={serviceContainerRepositoryDriverVersion.data?.version}
+                revision={serviceContainerRepositoryDriverVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.repositoryDriverRepository}
+                builtOn={serviceContainerRepositoryDriverVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem
+            title={
+              <a href={RepositoryUrls.rexRepository} target="_blank" rel="noopener noreferrer">
+                Rex
+              </a>
+            }
+          >
+            <ServiceContainerLoading {...serviceContainerRexVersion} variant="inline" title="Rex version">
+              <VersionText
+                version={serviceContainerRexVersion.data?.version}
+                revision={serviceContainerRexVersion.data?.commit}
+                repositoryUrl={RepositoryUrls.rexRepository}
+                builtOn={serviceContainerRexVersion.data?.builtOn}
+              />
+            </ServiceContainerLoading>
+          </AttributesItem>
+
+          <AttributesItem title="ETT">
+            <ServiceContainerLoading {...serviceContainerEttVersion} variant="inline" title="ETT version">
+              <VersionText
+                version={serviceContainerEttVersion.data?.version}
+                revision={serviceContainerEttVersion.data?.commit}
+                builtOn={serviceContainerEttVersion.data?.builtOn}
               />
             </ServiceContainerLoading>
           </AttributesItem>
