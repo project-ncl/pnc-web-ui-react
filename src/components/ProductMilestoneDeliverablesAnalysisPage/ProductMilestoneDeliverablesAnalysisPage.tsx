6ca1b013da6854ee14aa19e4e1a583652b44ab91
Patrik Korytar <patrik.korytar@gmail.com>
2024-03-07 13:24:08 +0100

    NCL-8427 Update Milestone Del Analysis list based on WS messages

--- src/components/ProductMilestoneDeliverablesAnalysisPage/ProductMilestoneDeliverablesAnalysisPage.tsx
+++ src/components/ProductMilestoneDeliverablesAnalysisPage/ProductMilestoneDeliverablesAnalysisPage.tsx
@@ -1,10 +1,22 @@
+import { useCallback } from 'react';
+
+import { DeliverableAnalyzerOperation } from 'pnc-api-types-ts';
+
+import { useComponentQueryParams } from 'hooks/useComponentQueryParams';
 import { useParamsRequired } from 'hooks/useParamsRequired';
+import {
+  hasDeliverablesAnalysisChanged,
+  hasDeliverablesAnalysisStarted,
+  usePncWebSocketEffect,
+} from 'hooks/usePncWebSocketEffect';
 import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 
 import { ProductMilestoneDeliverablesAnalysisList } from 'components/ProductMilestoneDeliverablesAnalysisList/ProductMilestoneDeliverablesAnalysisList';
 
 import * as productMilestoneApi from 'services/productMilestoneApi';
+
+import { refreshPage } from 'utils/refreshHelper';
 
 interface IProductMilestoneDeliverablesAnalysisPage {
   componentId?: string;
@@ -13,13 +25,38 @@ interface IProductMilestoneDeliverablesAnalysisPage {
 export const ProductMilestoneDeliverablesAnalysisPage = ({ componentId = 'c1' }: IProductMilestoneDeliverablesAnalysisPage) => {
   const { productMilestoneId } = useParamsRequired();
 
+  const { componentQueryParamsObject: deliverablesAnalysesQueryParamsObject } = useComponentQueryParams(componentId);
+
   const serviceContainerDeliverablesAnalysis = useServiceContainer(productMilestoneApi.getDeliverablesAnalysis);
   const serviceContainerDeliverablesAnalysisRunner = serviceContainerDeliverablesAnalysis.run;
+  const serviceContainerDeliverablesAnalysisSetter = serviceContainerDeliverablesAnalysis.setData;
 
   useQueryParamsEffect(
     ({ requestConfig } = {}) =>
       serviceContainerDeliverablesAnalysisRunner({ serviceData: { id: productMilestoneId }, requestConfig }),
     { componentId }
+  );
+
+  usePncWebSocketEffect(
+    useCallback(
+      (wsData: any) => {
+        if (hasDeliverablesAnalysisStarted(wsData, { productMilestoneId })) {
+          serviceContainerDeliverablesAnalysisRunner({
+            serviceData: { id: productMilestoneId },
+            requestConfig: { params: deliverablesAnalysesQueryParamsObject },
+          });
+        } else if (hasDeliverablesAnalysisChanged(wsData, { productMilestoneId })) {
+          const wsOperation: DeliverableAnalyzerOperation = wsData.operation;
+          serviceContainerDeliverablesAnalysisSetter((previousPage) => refreshPage(previousPage!, wsOperation));
+        }
+      },
+      [
+        serviceContainerDeliverablesAnalysisRunner,
+        serviceContainerDeliverablesAnalysisSetter,
+        deliverablesAnalysesQueryParamsObject,
+        productMilestoneId,
+      ]
+    )
   );
 
   return <ProductMilestoneDeliverablesAnalysisList {...{ serviceContainerDeliverablesAnalysis, componentId }} />;
