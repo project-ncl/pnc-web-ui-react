300369a6929f60ea43e12de84bfcc6d3ba632e11
Patrik Korytar <patrik.korytar@gmail.com>
2024-03-19 17:15:50 +0100

    NCL-8515 Apply Build bulk Artifact quality edit modal

--- src/components/BuildArtifactsPage/BuildArtifactsPage.tsx
+++ src/components/BuildArtifactsPage/BuildArtifactsPage.tsx
@@ -1,8 +1,15 @@
+import { Text, TextContent, ToolbarItem } from '@patternfly/react-core';
+import { useState } from 'react';
+
 import { useParamsRequired } from 'hooks/useParamsRequired';
 import { useQueryParamsEffect } from 'hooks/useQueryParamsEffect';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 
+import { ArtifactEditQualityModal } from 'components/ArtifactEditQualityModal/ArtifactEditQualityModal';
+import { ArtifactEditQualityModalButton } from 'components/ArtifactEditQualityModal/ArtifactEditQualityModalButton';
 import { ArtifactsList } from 'components/ArtifactsList/ArtifactsList';
+import { useServiceContainerBuild } from 'components/BuildPages/BuildPages';
+import { Toolbar } from 'components/Toolbar/Toolbar';
 
 import * as buildApi from 'services/buildApi';
 
@@ -13,13 +20,43 @@ interface IBuildArtifactsPageProps {
 export const BuildArtifactsPage = ({ componentId = 'a1' }: IBuildArtifactsPageProps) => {
   const { buildId } = useParamsRequired();
 
+  const { serviceContainerBuild } = useServiceContainerBuild();
+
   const serviceContainerArtifacts = useServiceContainer(buildApi.getBuiltArtifacts);
   const serviceContainerArtifactsRunner = serviceContainerArtifacts.run;
+
+  const [isEditQualitiesModalOpen, setIsEditQualitiesModalOpen] = useState<boolean>(false);
+
+  const toggleEditQualitiesModal = () => setIsEditQualitiesModalOpen((isEditQualitiesModalOpen) => !isEditQualitiesModalOpen);
 
   useQueryParamsEffect(
     ({ requestConfig } = {}) => serviceContainerArtifactsRunner({ serviceData: { id: buildId }, requestConfig }),
     { componentId }
   );
 
-  return <ArtifactsList {...{ serviceContainerArtifacts, componentId }} />;
+  return (
+    <>
+      <Toolbar>
+        <ToolbarItem>
+          <TextContent>
+            <Text component="h2">Artifacts</Text>
+          </TextContent>
+        </ToolbarItem>
+        <ToolbarItem>
+          <ArtifactEditQualityModalButton toggleModal={toggleEditQualitiesModal} variant="detail" />
+        </ToolbarItem>
+      </Toolbar>
+
+      <ArtifactsList {...{ serviceContainerArtifacts, componentId }} />
+
+      {isEditQualitiesModalOpen && (
+        <ArtifactEditQualityModal
+          isModalOpen={isEditQualitiesModalOpen}
+          toggleModal={toggleEditQualitiesModal}
+          build={serviceContainerBuild.data!}
+          variant="detail"
+        />
+      )}
+    </>
+  );
 };
