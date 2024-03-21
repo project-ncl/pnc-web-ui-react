7cb9964bdaffb2a772e4ab5ebc3575e51697288c
Martin Kelnar <mkelnar@redhat.com>
2024-03-21 13:09:17 +0100

    NCL-8519 Implement Logger Label support on Preferences page

--- src/components/PreferencesPage/PreferencesPage.tsx
+++ src/components/PreferencesPage/PreferencesPage.tsx
@@ -1,12 +1,24 @@
-import { Form, FormGroup, Switch } from '@patternfly/react-core';
-import { useEffect, useState } from 'react';
+import { Form, FormGroup, FormHelperText, Label, Switch, TextInput, TextInputProps } from '@patternfly/react-core';
+import { useEffect, useReducer, useState } from 'react';
+
+import { StorageKeys } from 'common/constants';
 
 import { useTitle } from 'hooks/useTitle';
 
 import { ContentBox } from 'components/ContentBox/ContentBox';
+import { ExpandableSection } from 'components/ExpandableSection/ExpandableSection';
 import { experimentalContentEnabledLocalStorageId } from 'components/ExperimentalContent/ExperimentalContent';
 import { PageLayout } from 'components/PageLayout/PageLayout';
 import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
+
+type TValidated = TextInputProps['validated'];
+
+interface ILoggerLabel {
+  value: string;
+  changed: boolean;
+  validated: TValidated;
+  error: boolean;
+}
 
 export const PreferencesPage = () => {
   const [isExperimentalContentEnabled, setIsExperimentalContentEnabled] = useState<boolean>(false);
@@ -18,9 +30,38 @@ export const PreferencesPage = () => {
 
   useTitle('Preferences');
 
+  const [showAdvanced, setShowAdvanced] = useState<boolean>(false);
+
+  const [loggerLabel, updateLoggerLabel] = useReducer(
+    (state: ILoggerLabel, value: string) => {
+      if (/^[a-zA-Z0-9-]{0,15}$/.test(value)) {
+        setShowAdvanced(true);
+        if (value) {
+          window.localStorage.setItem(StorageKeys.loggerLabel, `${value}`);
+        } else {
+          window.localStorage.removeItem(StorageKeys.loggerLabel);
+        }
+        return {
+          value,
+          changed: true,
+          validated: (value ? 'success' : 'default') as TValidated,
+          error: false,
+        };
+      }
+      console.log('Logger Label: invalid value');
+      return { ...state, error: true };
+    },
+    {
+      value: window.localStorage.getItem(StorageKeys.loggerLabel) ?? '',
+      changed: false,
+      validated: 'default',
+      error: false,
+    }
+  );
+
   return (
     <PageLayout title="Preferences" description="User preferences can be set here, including content and visual settings.">
-      <ContentBox padding>
+      <ContentBox padding marginBottom>
         <Form>
           <FormGroup
             label="Experimental content"
@@ -42,6 +83,49 @@ export const PreferencesPage = () => {
             />
           </FormGroup>
         </Form>
+      </ContentBox>
+
+      <ContentBox marginBottom background={false} shadow={false}>
+        <ExpandableSection
+          title="Advanced"
+          isExpanded={showAdvanced || !!loggerLabel.value}
+          onToggle={(isExpanded) => setShowAdvanced(isExpanded)}
+        >
+          <ContentBox padding>
+            <Form>
+              {/** Core features like {@link useForm} need to be avoided */}
+              <FormGroup
+                label="Logger Label"
+                fieldId="logger-label"
+                labelIcon={<TooltipWrapper tooltip="For debugging purposes only, leave it empty by default." />}
+                helperText={[
+                  <FormHelperText isHidden={!loggerLabel.value && !loggerLabel.error}>
+                    Up to 15 alphanumeric characters and dash symbol are allowed. It's recommended to use Jira number, such as{' '}
+                    <Label isCompact>NCL-1234</Label>
+                  </FormHelperText>,
+                  <FormHelperText isError isHidden={!loggerLabel.error}>
+                    The attempt to enter invalid value was rejected.
+                  </FormHelperText>,
+                ]}
+              >
+                <TextInput
+                  type="text"
+                  id="logger-label"
+                  name="logger-label"
+                  autoComplete="off"
+                  value={loggerLabel.value}
+                  validated={loggerLabel.validated}
+                  onChange={(value) => updateLoggerLabel(value)}
+                  onBlur={() => {
+                    if (loggerLabel.changed) {
+                      window.location.reload();
+                    }
+                  }}
+                />
+              </FormGroup>
+            </Form>
+          </ContentBox>
+        </ExpandableSection>
       </ContentBox>
     </PageLayout>
   );
