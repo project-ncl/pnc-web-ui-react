0c3d1e25343135b190406e9fa767fb240b48cc85
Patrik Korytar <patrik.korytar@gmail.com>
2024-04-11 13:02:21 +0200

    NCL-8635 Remove filtering and sorting from Artifact Milestone list

--- src/components/ArtifactProductMilestonesReleasesList/ArtifactProductMilestonesReleasesList.tsx
+++ src/components/ArtifactProductMilestonesReleasesList/ArtifactProductMilestonesReleasesList.tsx
@@ -1,23 +1,17 @@
 import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
-import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { MilestoneInfoPage } from 'pnc-api-types-ts';
 
 import { artifactProductMilestoneReleaseEntityAttributes } from 'common/artifactProductMilestoneReleaseEntityAttributes';
 import { PageTitles } from 'common/constants';
-import { getFilterOptions, getSortOptions } from 'common/entityAttributes';
 
 import { IServiceContainerState } from 'hooks/useServiceContainer';
-import { ISortOptions, useSorting } from 'hooks/useSorting';
 
 import { ContentBox } from 'components/ContentBox/ContentBox';
 import { DateTime } from 'components/DateTime/DateTime';
-import { Filtering } from 'components/Filtering/Filtering';
 import { Pagination } from 'components/Pagination/Pagination';
 import { ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
-import { Toolbar } from 'components/Toolbar/Toolbar';
-import { ToolbarItem } from 'components/Toolbar/ToolbarItem';
 import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
 
 interface IArtifactProductMilestonesReleasesListProps {
@@ -35,31 +29,9 @@ export const ArtifactProductMilestonesReleasesList = ({
   serviceContainerArtifactProductMilestonesReleases,
   componentId,
 }: IArtifactProductMilestonesReleasesListProps) => {
-  const sortOptions: ISortOptions = useMemo(
-    () =>
-      getSortOptions({
-        entityAttributes: artifactProductMilestoneReleaseEntityAttributes,
-      }),
-    []
-  );
-
-  const { getSortParams } = useSorting(sortOptions, componentId);
-
   return (
     <>
-      <Toolbar>
-        <ToolbarItem>
-          <Filtering
-            filterOptions={useMemo(
-              () => getFilterOptions({ entityAttributes: artifactProductMilestoneReleaseEntityAttributes }),
-              []
-            )}
-            componentId={componentId}
-          />
-        </ToolbarItem>
-      </Toolbar>
-
-      <ContentBox borderTop>
+      <ContentBox>
         <ServiceContainerLoading {...serviceContainerArtifactProductMilestonesReleases} title={PageTitles.productMilestones}>
           <TableComposable isStriped variant="compact">
             <Thead>
@@ -67,11 +39,9 @@ export const ArtifactProductMilestonesReleasesList = ({
                 <Th width={20}>{artifactProductMilestoneReleaseEntityAttributes.productName.title}</Th>
                 <Th width={15}>{artifactProductMilestoneReleaseEntityAttributes.productVersionVersion.title}</Th>
                 <Th width={15}>{artifactProductMilestoneReleaseEntityAttributes.milestoneVersion.title}</Th>
-                <Th width={15} sort={getSortParams(sortOptions.sortAttributes.milestoneEndDate.id)}>
-                  {artifactProductMilestoneReleaseEntityAttributes.milestoneEndDate.title}
-                </Th>
+                <Th width={15}>{artifactProductMilestoneReleaseEntityAttributes.milestoneEndDate.title}</Th>
                 <Th width={15}>{artifactProductMilestoneReleaseEntityAttributes.releaseVersion.title}</Th>
-                <Th width={15} sort={getSortParams(sortOptions.sortAttributes.releaseReleaseDate.id)}>
+                <Th width={15}>
                   {artifactProductMilestoneReleaseEntityAttributes.releaseReleaseDate.title}
                   <TooltipWrapper tooltip={artifactProductMilestoneReleaseEntityAttributes.releaseReleaseDate.tooltip} />
                 </Th>
