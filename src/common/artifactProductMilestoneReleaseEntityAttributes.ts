000196128f3cf97e401f7f1814fb36e8d9f6008c
Patrik Korytar <patrik.korytar@gmail.com>
2024-04-11 13:02:21 +0200

    NCL-8635 Remove filtering and sorting from Artifact Milestone list

--- src/common/artifactProductMilestoneReleaseEntityAttributes.ts
+++ src/common/artifactProductMilestoneReleaseEntityAttributes.ts
@@ -10,32 +10,26 @@ export const artifactProductMilestoneReleaseEntityAttributes = {
   productName: {
     id: 'productName',
     title: `Product ${productEntityAttributes.name.title}`,
-    filter: productEntityAttributes.name.filter,
   },
   productVersionVersion: {
     id: 'productVersionVersion',
     title: `Product ${productVersionEntityAttributes.version.title}`,
-    filter: productVersionEntityAttributes.version.filter,
   },
   milestoneVersion: {
     id: 'milestoneVersion',
     title: `Milestone ${productMilestoneEntityAttributes.version.title}`,
-    filter: productMilestoneEntityAttributes.version.filter,
   },
   milestoneEndDate: {
     id: 'milestoneEndDate',
     title: `Milestone ${productMilestoneEntityAttributes.endDate.title}`,
-    sort: {},
   },
   releaseVersion: {
     id: 'releaseVersion',
     title: `Release ${productReleaseEntityAttributes.version.title}`,
-    filter: productReleaseEntityAttributes.version.filter,
   },
   releaseReleaseDate: {
     id: 'releaseReleaseDate',
     title: productReleaseEntityAttributes.releaseDate.title,
     tooltip: productReleaseEntityAttributes.releaseDate.tooltip,
-    sort: {},
   },
 } as const satisfies TEntityAttributes<MilestoneInfo>;
