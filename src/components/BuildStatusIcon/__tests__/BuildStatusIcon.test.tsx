d6e0b97e25c9377f1c63962333980686332e3a04
Patrik Korytar <patrik.korytar@gmail.com>
2024-04-11 14:16:21 +0200

    NCL-8506 Provide Build Log link on Status Icon

--- src/components/BuildStatusIcon/__tests__/BuildStatusIcon.test.tsx
+++ src/components/BuildStatusIcon/__tests__/BuildStatusIcon.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { Build, GroupBuild } from 'pnc-api-types-ts';
 
@@ -8,6 +9,12 @@ import mockBuildData from './data/mock-build-data.json';
 
 describe('display BuildStatusIcon component', () => {
   test('renders all BuildStatusIcon variants', () => {
-    (mockBuildData as (Build | GroupBuild)[]).forEach((build) => render(<BuildStatusIcon build={build} />));
+    (mockBuildData as (Build | GroupBuild)[]).forEach((build) =>
+      render(
+        <MemoryRouter>
+          <BuildStatusIcon build={build} />
+        </MemoryRouter>
+      )
+    );
   });
 });
