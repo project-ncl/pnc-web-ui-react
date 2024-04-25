39b2e31efdf3e5409ef12cb29dcce30c242f6409
Patrik Korytar <patrik.korytar@gmail.com>
2024-04-25 12:43:30 +0200

    NCL-8677 Add disabled prop to Page tabs item component

--- src/components/PageTabs/PageTabsItem.tsx
+++ src/components/PageTabs/PageTabsItem.tsx
@@ -3,18 +3,30 @@ import styles from '@patternfly/react-styles/css/components/Tabs/tabs';
 import { PropsWithChildren } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
+
+import pageTabsItemStyles from './PageTabsItem.module.css';
+
 interface IPageTabsItemProps {
   url: string;
+  isDisabled?: boolean;
+  tooltip?: string;
 }
 
-export const PageTabsItem = ({ children, url }: PropsWithChildren<IPageTabsItemProps>) => {
+export const PageTabsItem = ({ children, url, isDisabled = false, tooltip }: PropsWithChildren<IPageTabsItemProps>) => {
   const { pathname } = useLocation();
 
   return (
     <li className={css(styles.tabsItem, pathname.includes(url) && styles.modifiers.current)}>
-      <Link to={url} className={styles.tabsLink}>
-        <span className={styles.tabsItemText}>{children}</span>
-      </Link>
+      <TooltipWrapper tooltip={tooltip}>
+        <Link
+          to={url}
+          className={css(styles.tabsLink, isDisabled && styles.modifiers.ariaDisabled, pageTabsItemStyles['page-tabs-item_link'])}
+          aria-disabled={isDisabled}
+        >
+          <span className={styles.tabsItemText}>{children}</span>
+        </Link>
+      </TooltipWrapper>
     </li>
   );
 };
