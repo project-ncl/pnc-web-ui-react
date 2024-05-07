037078cc7a4075f06284995422ddb81afe16a746
Patrik Korytar <patrik.korytar@gmail.com>
2024-05-07 11:34:48 +0200

    NCL-8129 Extend IconWrapper for medium-size icon variants and fix size lengths

--- src/components/IconWrapper/IconWrapper.tsx
+++ src/components/IconWrapper/IconWrapper.tsx
@@ -1,10 +1,25 @@
+import { css } from '@patternfly/react-styles';
 import { PropsWithChildren } from 'react';
 
 import styles from './IconWrapper.module.css';
+
+interface IIconWrapperProps {
+  variant: 'small' | 'medium';
+}
 
 /**
  * Icon Wrapper aims to set minimal width to icons and stop them disapearing with resizing.
  *
  * @param children - icon to display
  */
-export const IconWrapper = ({ children }: PropsWithChildren<{}>) => <span className={styles['icon-wrapper']}>{children}</span>;
+export const IconWrapper = ({ children, variant }: PropsWithChildren<IIconWrapperProps>) => (
+  <span
+    className={css(
+      styles['icon-wrapper'],
+      variant === 'small' && styles['icon-wrapper--small'],
+      variant === 'medium' && styles['icon-wrapper--medium']
+    )}
+  >
+    {children}
+  </span>
+);
