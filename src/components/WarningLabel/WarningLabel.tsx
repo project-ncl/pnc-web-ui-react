3e690af26741e339247c89b369aedd9375872967
Patrik Korytar <patrik.korytar@gmail.com>
2024-05-13 14:16:50 +0200

    NCL-8640 Add deprecated Environment warning icon to BC list

--- src/components/WarningLabel/WarningLabel.tsx
+++ src/components/WarningLabel/WarningLabel.tsx
@@ -1,12 +1,13 @@
 import { Label, LabelProps } from '@patternfly/react-core';
-import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+
+import { WarningIcon } from 'components/Icons/WarningIcon';
 
 interface IWarningLabelProps extends Exclude<LabelProps, 'color' | 'icon'> {
   hasIcon?: boolean;
 }
 
 export const WarningLabel = ({ hasIcon = false, children, ...otherProps }: IWarningLabelProps) => (
-  <Label color="gold" icon={hasIcon && <ExclamationTriangleIcon />} {...otherProps}>
+  <Label color="gold" icon={hasIcon && <WarningIcon />} {...otherProps}>
     {children}
   </Label>
 );
