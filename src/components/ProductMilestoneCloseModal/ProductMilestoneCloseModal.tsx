0dfa4f5c486a36d0c953053e0fc6478652a54027
Patrik Korytar <patrik.korytar@gmail.com>
2024-05-21 11:11:33 +0200

    NCL-8705 Fix Milestone close action on-success link route

--- src/components/ProductMilestoneCloseModal/ProductMilestoneCloseModal.tsx
+++ src/components/ProductMilestoneCloseModal/ProductMilestoneCloseModal.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { ProductMilestone } from 'pnc-api-types-ts';
 
+import { useParamsRequired } from 'hooks/useParamsRequired';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 
 import { ActionModal } from 'components/ActionModal/ActionModal';
@@ -16,6 +17,8 @@ export interface IProductMilestoneCloseModalProps {
 }
 
 export const ProductMilestoneCloseModal = ({ isModalOpen, toggleModal, productMilestone }: IProductMilestoneCloseModalProps) => {
+  const { productId, productVersionId } = useParamsRequired();
+
   const serviceContainerProductMilestoneClose = useServiceContainer(productMilestoneApi.closeProductMilestone, 0);
 
   const confirmModal = () => {
@@ -42,8 +45,13 @@ export const ProductMilestoneCloseModal = ({ isModalOpen, toggleModal, productMi
         <Button
           key="close-result-link"
           variant="secondary"
-          // TODO: Make link absolute once Product data are available
-          component={(props) => <Link {...props} to={`close-results/${serviceContainerProductMilestoneClose.data?.id}`} />}
+          // TODO: Use props from Milestone DTO once Product data are available
+          component={(props) => (
+            <Link
+              {...props}
+              to={`/products/${productId}/versions/${productVersionId}/milestones/${productMilestone.id}/close-results/${serviceContainerProductMilestoneClose.data?.id}`}
+            />
+          )}
         >
           Open Close Result
         </Button>,
