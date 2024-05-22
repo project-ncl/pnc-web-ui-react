55dd67a01f6ec29d43952dfd00f70c9308b7c019
Martin Kelnar <mkelnar@redhat.com>
2024-05-22 09:41:31 +0200

    NCL-8706 Add Group Build link to Build page

---
+++ src/components/GroupBuildLink/GroupBuildLink.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+interface IGroupBuildLinkProps {
+  id: string;
+  children?: React.ReactNode;
+}
+
+/**
+ * Represents a link to a GroupBuildDetailPage of a specific Group Build
+ *
+ * @param id - id of the Group Build to link to
+ */
+export const GroupBuildLink = ({ id, children }: IGroupBuildLinkProps) => <Link to={`/group-builds/${id}`}>{children}</Link>;
