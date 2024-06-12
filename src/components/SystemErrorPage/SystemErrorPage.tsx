4fad41cacb382431516a00742a0124a7f2091942
Patrik Korytar <patrik.korytar@gmail.com>
2024-06-12 14:31:52 +0200

    NCL-7537 Update PF CSS class and variable names to v5 version

--- src/components/SystemErrorPage/SystemErrorPage.tsx
+++ src/components/SystemErrorPage/SystemErrorPage.tsx
@@ -10,11 +10,11 @@ import { CubesIcon } from '@patternfly/react-icons';
 
 export const SystemErrorPage = () => {
   return (
-    <EmptyState variant={EmptyStateVariant.xl} className="pf-u-pt-4xl">
+    <EmptyState variant={EmptyStateVariant.xl} className="pf-v5-u-pt-4xl">
       <EmptyStateHeader titleText="System Error" icon={<EmptyStateIcon icon={CubesIcon} />} headingLevel="h1" />
       <EmptyStateBody>Unexpected error occurred, report has been sent to the server.</EmptyStateBody>
       <EmptyStateFooter>
-        <a href="/" className="pf-c-button pf-m-primary">
+        <a href="/" className="pf-v5-c-button pf-m-primary">
           Return to Dashboard
         </a>
       </EmptyStateFooter>
