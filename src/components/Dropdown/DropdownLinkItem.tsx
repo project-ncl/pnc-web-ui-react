a5a7193b5abab08070d451c07eca89cf5d1d280b
Patrik Korytar <patrik.korytar@gmail.com>
2024-06-19 14:31:22 +0200

    NCL-7537 Implement Dropdown item with router link

---
+++ src/components/Dropdown/DropdownLinkItem.tsx
@@ -0,0 +1,13 @@
+import { DropdownItem, DropdownItemProps } from '@patternfly/react-core';
+import { PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
+
+interface IDropdownLinkItemProps extends DropdownItemProps {
+  to: string;
+}
+
+export const DropdownLinkItem = ({ to, children, ...restDropdownItemProps }: PropsWithChildren<IDropdownLinkItemProps>) => (
+  <DropdownItem component={(props: any) => <Link {...props} to={to} />} {...restDropdownItemProps}>
+    {children}
+  </DropdownItem>
+);
