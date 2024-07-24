9f1e512365e11260c07814d9f687cabdcad62836
Dennis Zhou <tozhou@redhat.com>
2024-07-24 02:08:21 +0100

    NCL-8755 Show SSH details for PNC admins

--- src/components/BuildLogPage/BuildLogPage.tsx
+++ src/components/BuildLogPage/BuildLogPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useMemo } from 'react';
 
+import { buildStatusData } from 'common/buildStatusData';
+
 import { useParamsRequired } from 'hooks/useParamsRequired';
 import { useServiceContainer } from 'hooks/useServiceContainer';
 
@@ -9,8 +11,10 @@ import { ContentBox } from 'components/ContentBox/ContentBox';
 import { LogViewer } from 'components/LogViewer/LogViewer';
 import { OldUiContentLinkBox } from 'components/OldUiContentLinkBox/OldUiContentLinkBox';
 import { ServiceContainerLoading } from 'components/ServiceContainers/ServiceContainerLoading';
+import { SshCredentialsButton } from 'components/SshCredentialsButton/SshCredentialsButton';
 
 import * as buildApi from 'services/buildApi';
+import { userService } from 'services/userService';
 
 export const BuildLogPage = () => {
   const { buildId } = useParamsRequired();
@@ -21,6 +25,15 @@ export const BuildLogPage = () => {
   const serviceContainerBuildLog = useServiceContainer(buildApi.getBuildLog);
   const serviceContainerBuildLogRunner = serviceContainerBuildLog.run;
 
+  const serviceContainerBuildSshCredentials = useServiceContainer(buildApi.getSshCredentials);
+  const serviceContainerBuildSshCredentialsRunner = serviceContainerBuildSshCredentials.run;
+
+  const buildBelongToCurrentUser = useMemo(
+    () => userService.getUserId() === serviceContainerBuild.data?.user?.id,
+    [serviceContainerBuild.data]
+  );
+  const isCurrentUserAdmin = useMemo(() => userService.isAdminUser(), []);
+
   const logData = useMemo(() => serviceContainerBuildLog.data?.split(/[\r\n]/) || [], [serviceContainerBuildLog.data]);
 
   useEffect(() => {
@@ -29,7 +42,31 @@ export const BuildLogPage = () => {
     }
   }, [serviceContainerBuildLogRunner, buildId, isBuilding]);
 
-  const logActions = [<BuildLogLink key="log-link" buildId={buildId!} />];
+  useEffect(() => {
+    if (
+      (buildBelongToCurrentUser || isCurrentUserAdmin) &&
+      serviceContainerBuild.data?.status &&
+      buildStatusData[serviceContainerBuild.data.status].failed
+    ) {
+      serviceContainerBuildSshCredentialsRunner({ serviceData: { id: buildId } });
+    }
+  }, [
+    serviceContainerBuildSshCredentialsRunner,
+    buildId,
+    buildBelongToCurrentUser,
+    serviceContainerBuild.data?.status,
+    isCurrentUserAdmin,
+  ]);
+
+  const logActions = [
+    <SshCredentialsButton
+      key="ssh-credentials"
+      serviceContainerSshCredentials={serviceContainerBuildSshCredentials}
+      buildBelongToCurrentUser={buildBelongToCurrentUser || isCurrentUserAdmin}
+      hasBuildFailed={!!serviceContainerBuild.data?.status && !!buildStatusData[serviceContainerBuild.data.status].failed}
+    />,
+    <BuildLogLink key="log-link" buildId={buildId!} />,
+  ];
 
   return (
     <>
