99391df549f8abbcf2596fc72a17e9de5dd348c8
Patrik Korytar <patrik.korytar@gmail.com>
2024-09-25 16:25:00 +0200

    NCL-8835 Add icon button variant to BuildLogLink component

--- src/components/BuildLogLink/BuildLogLink.tsx
+++ src/components/BuildLogLink/BuildLogLink.tsx
@@ -1,12 +1,28 @@
+import { Button } from '@patternfly/react-core';
+import { ExternalLinkAltIcon } from '@patternfly/react-icons';
+
+import { TooltipWrapper } from 'components/TooltipWrapper/TooltipWrapper';
+
 import * as webConfigService from 'services/webConfigService';
 
 interface IBuildLogLinkProps {
   buildId: string;
+  isIconVariant?: boolean;
   title?: string;
 }
 
-export const BuildLogLink = ({ buildId, title = 'view' }: IBuildLogLinkProps) => (
-  <a target="_blank" rel="noopener noreferrer" href={`${webConfigService.getPncUrl()}/builds/${buildId}/logs/build`}>
-    {title}
-  </a>
-);
+export const BuildLogLink = ({ buildId, isIconVariant = false, title = 'View raw' }: IBuildLogLinkProps) => {
+  const buildLogUrl = `${webConfigService.getPncUrl()}/builds/${buildId}/logs/build`;
+
+  return isIconVariant ? (
+    <TooltipWrapper tooltip={title}>
+      <Button variant="plain" target="_blank" rel="noopener noreferrer" component="a" href={buildLogUrl} style={{ padding: 0 }}>
+        <ExternalLinkAltIcon /> {title}
+      </Button>
+    </TooltipWrapper>
+  ) : (
+    <a target="_blank" rel="noopener noreferrer" href={buildLogUrl}>
+      {title}
+    </a>
+  );
+};
