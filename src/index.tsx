9e221b3382c713f3554cbe3e3dbf04fe4b5ab83b
Jozef Mrazek <jmrazek@redhat.com>
2025-04-08 15:31:20 +0200

    Upgrade path v7_normalizeFormMethod

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,7 @@ const router = createBrowserRouter(createRoutesFromElements(AppRoutes), {
   future: {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
   },
 });
 
