471df0e9a51c4a32bfa8a9ed0d8d91997c9975c7
Patrik Korytar <patrik.korytar@gmail.com>
2025-04-09 15:26:58 +0200

    NCL-8075 Implement Deliverable Analysis label edit

--- src/services/deliverableAnalysisApi.ts
+++ src/services/deliverableAnalysisApi.ts
@@ -2,6 +2,8 @@ import { AxiosRequestConfig } from 'axios';
 
 import { DeliverableAnalyzerReport } from 'pnc-api-types-ts';
 
+import { DeliverableAnalysisLabel } from 'common/deliverableAnalysisLabelEntryEntityAttributes';
+
 import { pncClient } from 'services/pncClient';
 
 interface IDeliverableAnalysisReportApiData {
@@ -20,4 +22,39 @@ export const getDeliverableAnalysisReport = (
   requestConfig: AxiosRequestConfig = {}
 ) => {
   return pncClient.getHttpClient().get<DeliverableAnalyzerReport>(`/deliverable-analyses/${id}`, requestConfig);
+};
+
+interface IDeliverableAnalysisReportEditLabelApiData {
+  id: string;
+  data: { label: DeliverableAnalysisLabel; reason: string };
+}
+
+/**
+ * Adds a label to the Deliverable Analysis report.
+ *
+ * @param serviceData - object containing:
+ *  - id - Deliverable Analysis ID
+ *  - data - label and the reason for the change
+ * @param requestConfig - Axios based request config
+ */
+export const addDeliverableAnalysisLabel = (
+  { id, data }: IDeliverableAnalysisReportEditLabelApiData,
+  requestConfig: AxiosRequestConfig = {}
+) => {
+  return pncClient.getHttpClient().post<undefined>(`/deliverable-analyses/${id}/add-label`, data, requestConfig);
+};
+
+/**
+ * Removes a label from the Deliverable Analysis report.
+ *
+ * @param serviceData - object containing:
+ *  - id - Deliverable Analysis ID
+ *  - data - label and the reason for the change
+ * @param requestConfig - Axios based request config
+ */
+export const removeDeliverableAnalysisLabel = (
+  { id, data }: IDeliverableAnalysisReportEditLabelApiData,
+  requestConfig: AxiosRequestConfig = {}
+) => {
+  return pncClient.getHttpClient().post<undefined>(`/deliverable-analyses/${id}/remove-label`, data, requestConfig);
 };
