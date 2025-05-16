a9677d496144c49f8da38f741462730d4b595fb2
tozhou <tozhou@redhat.com>
2025-05-16 13:03:21 +0100

    NCL-8076 Remove wrong semicolon

--- src/components/DeliverableAnalysisDeliveredArtifactsPage/DeliverableAnalysisDeliveredArtifactsPage.tsx
+++ src/components/DeliverableAnalysisDeliveredArtifactsPage/DeliverableAnalysisDeliveredArtifactsPage.tsx
@@ -43,7 +43,7 @@ export const DeliverableAnalysisDeliveredArtifactsPage = ({
           </TextContent>
         </ToolbarItem>
       </Toolbar>
-      <AnalyzedArtifactsList {...{ serviceContainerArtifacts, componentId }} />;
+      <AnalyzedArtifactsList {...{ serviceContainerArtifacts, componentId }} />
     </>
   );
 };
