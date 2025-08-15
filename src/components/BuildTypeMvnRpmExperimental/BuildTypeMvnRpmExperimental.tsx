6570de469e96a072d85e1d3d9fe8a51a123c39a6
Martin Kelnar <mkelnar@redhat.com>
2025-08-15 10:45:38 +0200

    NCL-9226 Create MVN RPM experimental components

---
+++ src/components/BuildTypeMvnRpmExperimental/BuildTypeMvnRpmExperimental.tsx
@@ -0,0 +1,42 @@
+import { Icon, Tooltip } from '@patternfly/react-core';
+import { ExclamationTriangleIcon, ExternalLinkAltIcon } from '@patternfly/react-icons';
+
+import { buildTypeData } from 'common/buildTypeData';
+
+import * as webConfigService from 'services/webConfigService';
+
+export const BuildTypeMvnRpmExperimentalIcon = () => {
+  return (
+    <Icon status="warning">
+      <ExclamationTriangleIcon />
+    </Icon>
+  );
+};
+
+export const BuildTypeMvnRpmExperimentalDescription = () => {
+  const webConfig = webConfigService.getWebConfig();
+
+  return (
+    <>
+      {buildTypeData.MVN_RPM.id} feature is experimental. Feedback is being collected to evaluate and improve it. You may create a
+      new&nbsp;&nbsp;
+      <a href={webConfig.userGuideUrl} target="_blank" rel="noopener noreferrer">
+        feature request <ExternalLinkAltIcon />
+      </a>
+      &nbsp;&nbsp;or contact&nbsp;&nbsp;
+      <a href={webConfig.userSupportUrl} target="_blank" rel="noopener noreferrer">
+        user's support <ExternalLinkAltIcon />
+      </a>
+    </>
+  );
+};
+
+export const BuildTypeMvnRpmExperimentalTooltip = () => {
+  return (
+    <span className="p-l-5">
+      <Tooltip content={<BuildTypeMvnRpmExperimentalDescription />}>
+        <BuildTypeMvnRpmExperimentalIcon />
+      </Tooltip>
+    </span>
+  );
+};
