3da097c8aa14d6d0c4e54c169275bde656897880
Martin Kelnar <mkelnar@redhat.com>
2026-07-01 11:40:26 +0000

    NCL-9783 Add displayTooltip to Build Type label mapper

--- src/components/LabelMapper/BuildConfigBuildTypeLabelMapper.tsx
+++ src/components/LabelMapper/BuildConfigBuildTypeLabelMapper.tsx
@@ -7,13 +7,14 @@ import { LabelMapper } from 'components/LabelMapper/LabelMapper';
 
 interface IBuildConfigBuildTypeLabelMapperProps {
   buildType: BuildConfiguration['buildType'];
+  displayTooltip?: boolean;
 }
 
-export const BuildConfigBuildTypeLabelMapper = ({ buildType }: IBuildConfigBuildTypeLabelMapperProps) => {
+export const BuildConfigBuildTypeLabelMapper = ({ buildType, displayTooltip = false }: IBuildConfigBuildTypeLabelMapperProps) => {
   const config = buildTypeColorMap[buildType] ?? { text: buildType };
   return (
     <>
-      <LabelMapper mapperItem={config} />
+      <LabelMapper mapperItem={config} tooltip={displayTooltip ? 'Build Type' : undefined} />
       {buildType === buildTypeData.MVN_RPM.id ? <BuildTypeMvnRpmExperimentalTooltip /> : undefined}
     </>
   );
